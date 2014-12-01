228ea96571b18238e80526563cb2a8006176ed51
imd-solutions <admin@imarveldesign.co.uk>
2014-12-01 17:35:17 +0000

    Data Cleanse

--- data_cleanse/db_connect.php
+++ data_cleanse/db_connect.php
@@ -1,7 +1,6 @@
 <?php
 
-//$link = mysql_connect('109.203.125.38', 'gsmstock_admin', 'zv.4qAb17ph$;?$PF!') or die("Database Error");	
-$link = mysql_connect('localhost', 'root', 'Rwt189K72');
+//$link = mysql_connect('109.203.125.38', 'gsmstock_admin', 'zv.4qAb17ph$;?$PF!') or die("Database Error");
 
 if(! $link )
 {
