1cdfadca8549e38550748182605f7aaa00994331
imd-solutions <admin@imarveldesign.co.uk>
2014-12-01 13:51:20 +0000

    Data Cleanse

--- data_cleanse/email_source.php
+++ data_cleanse/email_source.php
@@ -3,8 +3,13 @@
 //connect to the database 
 $link = mysql_connect('109.203.125.38', 'gsmstock_admin', 'zv.4qAb17ph$;?$PF!') or die("Database Error");	
  mysql_select_db('gsmstock_generator', $link);
+ 
+$result = mysql_query("SELECT * FROM emails");
+$num_rows = mysql_num_rows($result);
 // 
 $source = mysql_real_escape_string($_POST['source']);
+$count = 1;
+$delete_count = 0;
 if ($_FILES[csv][size] > 0) { 
 
     //get the csv file
@@ -15,21 +20,40 @@ if ($_FILES[csv][size] > 0) {
     //loop through the csv file and insert into database 
     do { 
         if ($data[0]) { 
-            mysql_query("INSERT INTO emails (email_address, source, date_created) VALUES 
-                ( 
-                    '".addslashes($data[0])."', 
-                    '".$source."',
-                    '".date('Y-m-d H:i:s')."'
-                ) 
-            ") or die (mysql_error());
+            
+            $dupesql = "SELECT email_address FROM gsmstock_master.master_data WHERE email_address = ".$data[0]."";
+
+            $duperaw = mysql_query($dupesql);
+
+            if (mysql_num_rows($duberaw) < 0) {
+              //your code ...
+                    mysql_query("INSERT INTO emails (email_address, source, date_created) VALUES 
+                        ( 
+                            '".addslashes($data[0])."', 
+                            '".$source."',
+                            '".date('Y-m-d H:i:s')."'
+                        ) 
+                    ") or die (mysql_error());
+                    $count++;
+            }
+            else{
+                $delete_count++;
+            }
+            
         } 
     } while ($data = fgetcsv($handle,1000,",","'")); 
     // 
 
-    $message = '<span style="color:#e24139;">That has been processed successfully</span>';
-    mysql_close($link);
+    
 
 }
+$message = '<span style="color:#e24139;">
+            Current Database size: '.$num_rows.'<br/>
+            CSV Records Uploaded: '.$cv_count.'<br/>
+            Total number of records successfully added: '.$count.'<br/>
+            Duplicates: '.$delete_count.'<br/>
+            </span>';
+mysql_close($link);
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
