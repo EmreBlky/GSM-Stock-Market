2570077c480c1db96dc29f5352c9b0d1ca45d7c8
Daniel Gregory <mail@danielgregory.me>
2015-01-13 14:37:53 +0000

    Update

--- application/modules/footer/views/footer.php
+++ application/modules/footer/views/footer.php
@@ -3,7 +3,7 @@
                 10GB of <strong>250GB</strong> Free.
             </div>
             <div>
-                <strong>Copyright</strong> Example Company &copy; 2014-2015
+                <strong>Copyright</strong> GSM Stock Market Limited &copy; <?php echo date("Y"); ?>
             </div>
                                
         </div>
