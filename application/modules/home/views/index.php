1e1c23e93fbe8023b9b2b9f71d64cef92b1acb2b
Daniel Gregory <mail@danielgregory.me>
2015-01-26 14:38:29 +0000

    Update

--- application/modules/home/views/index.php
+++ application/modules/home/views/index.php
@@ -227,7 +227,7 @@
                           <div class="col-lg-12">
                                 <div class="ibox float-e-margins">
                                     <div class="ibox-title">
-                                        <h5>User project list</h5>
+                                        <h5>Buying Requests</h5>
                                         <div class="ibox-tools">
                                             <a class="collapse-link">
                                                 <i class="fa fa-chevron-up"></i>
@@ -241,54 +241,55 @@
                                         <table class="table table-hover no-margins">
                                             <thead>
                                             <tr>
+                                            	<th>Make</th>
+                                                <th>Model</th>
+                                                <th>Qty</th>
+                                                <th>Price</th>
                                                 <th>Status</th>
-                                                <th>Date</th>
-                                                <th>User</th>
-                                                <th>Value</th>
                                             </tr>
                                             </thead>
                                             <tbody>
                                             <tr>
-                                                <td><small>Pending...</small></td>
-                                                <td><i class="fa fa-clock-o"></i> 11:20pm</td>
-                                                <td>Samantha</td>
-                                                <td class="text-navy"> <i class="fa fa-level-up"></i> 24% </td>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-primary">Active</span></td>
                                             </tr>
                                             <tr>
-                                                <td><span class="label label-warning">Canceled</span> </td>
-                                                <td><i class="fa fa-clock-o"></i> 10:40am</td>
-                                                <td>Monica</td>
-                                                <td class="text-navy"> <i class="fa fa-level-up"></i> 66% </td>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-primary">Active/span></td>
                                             </tr>
                                             <tr>
-                                                <td><small>Pending...</small> </td>
-                                                <td><i class="fa fa-clock-o"></i> 01:30pm</td>
-                                                <td>John</td>
-                                                <td class="text-navy"> <i class="fa fa-level-up"></i> 54% </td>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-success">Sold</span></td>
                                             </tr>
                                             <tr>
-                                                <td><small>Pending...</small> </td>
-                                                <td><i class="fa fa-clock-o"></i> 02:20pm</td>
-                                                <td>Agnes</td>
-                                                <td class="text-navy"> <i class="fa fa-level-up"></i> 12% </td>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-success">Sold</span></td>
                                             </tr>
                                             <tr>
-                                                <td><small>Pending...</small> </td>
-                                                <td><i class="fa fa-clock-o"></i> 09:40pm</td>
-                                                <td>Janet</td>
-                                                <td class="text-navy"> <i class="fa fa-level-up"></i> 22% </td>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-warning">Pending</span></td>
                                             </tr>
                                             <tr>
-                                                <td><span class="label label-primary">Completed</span> </td>
-                                                <td><i class="fa fa-clock-o"></i> 04:10am</td>
-                                                <td>Amelia</td>
-                                                <td class="text-navy"> <i class="fa fa-level-up"></i> 66% </td>
-                                            </tr>
-                                            <tr>
-                                                <td><small>Pending...</small> </td>
-                                                <td><i class="fa fa-clock-o"></i> 12:08am</td>
-                                                <td>Damian</td>
-                                                <td class="text-navy"> <i class="fa fa-level-up"></i> 23% </td>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-danger">Cancelled</span></td>
                                             </tr>
                                             </tbody>
                                         </table>
@@ -299,7 +300,7 @@
                             <div class="col-lg-12">
                                 <div class="ibox float-e-margins">
                                     <div class="ibox-title">
-                                        <h5>User project list</h5>
+                                        <h5>Selling Offers</h5>
                                         <div class="ibox-tools">
                                             <a class="collapse-link">
                                                 <i class="fa fa-chevron-up"></i>
@@ -313,54 +314,128 @@
                                         <table class="table table-hover no-margins">
                                             <thead>
                                             <tr>
+                                            	<th>Make</th>
+                                                <th>Model</th>
+                                                <th>Qty</th>
+                                                <th>Price</th>
                                                 <th>Status</th>
-                                                <th>Date</th>
-                                                <th>User</th>
-                                                <th>Value</th>
                                             </tr>
                                             </thead>
                                             <tbody>
                                             <tr>
-                                                <td><small>Pending...</small></td>
-                                                <td><i class="fa fa-clock-o"></i> 11:20pm</td>
-                                                <td>Samantha</td>
-                                                <td class="text-navy"> <i class="fa fa-level-up"></i> 24% </td>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-primary">Active</span></td>
                                             </tr>
                                             <tr>
-                                                <td><span class="label label-warning">Canceled</span> </td>
-                                                <td><i class="fa fa-clock-o"></i> 10:40am</td>
-                                                <td>Monica</td>
-                                                <td class="text-navy"> <i class="fa fa-level-up"></i> 66% </td>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-primary">Active/span></td>
                                             </tr>
                                             <tr>
-                                                <td><small>Pending...</small> </td>
-                                                <td><i class="fa fa-clock-o"></i> 01:30pm</td>
-                                                <td>John</td>
-                                                <td class="text-navy"> <i class="fa fa-level-up"></i> 54% </td>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-success">Sold</span></td>
                                             </tr>
                                             <tr>
-                                                <td><small>Pending...</small> </td>
-                                                <td><i class="fa fa-clock-o"></i> 02:20pm</td>
-                                                <td>Agnes</td>
-                                                <td class="text-navy"> <i class="fa fa-level-up"></i> 12% </td>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-success">Sold</span></td>
                                             </tr>
                                             <tr>
-                                                <td><small>Pending...</small> </td>
-                                                <td><i class="fa fa-clock-o"></i> 09:40pm</td>
-                                                <td>Janet</td>
-                                                <td class="text-navy"> <i class="fa fa-level-up"></i> 22% </td>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-warning">Pending</span></td>
                                             </tr>
                                             <tr>
-                                                <td><span class="label label-primary">Completed</span> </td>
-                                                <td><i class="fa fa-clock-o"></i> 04:10am</td>
-                                                <td>Amelia</td>
-                                                <td class="text-navy"> <i class="fa fa-level-up"></i> 66% </td>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-danger">Cancelled</span></td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-lg-12">
+                                <div class="ibox float-e-margins">
+                                    <div class="ibox-title">
+                                        <h5>Watch List</h5>
+                                        <div class="ibox-tools">
+                                            <a class="collapse-link">
+                                                <i class="fa fa-chevron-up"></i>
+                                            </a>
+                                            <a class="close-link">
+                                                <i class="fa fa-times"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="ibox-content">
+                                        <table class="table table-hover no-margins">
+                                            <thead>
+                                            <tr>
+                                            	<th>Make</th>
+                                                <th>Model</th>
+                                                <th>Qty</th>
+                                                <th>Price</th>
+                                                <th>Status</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-primary">Active</span></td>
                                             </tr>
                                             <tr>
-                                                <td><small>Pending...</small> </td>
-                                                <td><i class="fa fa-clock-o"></i> 12:08am</td>
-                                                <td>Damian</td>
-                                                <td class="text-navy"> <i class="fa fa-level-up"></i> 23% </td>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-primary">Active/span></td>
+                                            </tr>
+                                            <tr>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-success">Sold</span></td>
+                                            </tr>
+                                            <tr>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-success">Sold</span></td>
+                                            </tr>
+                                            <tr>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-warning">Pending</span></td>
+                                            </tr>
+                                            <tr>
+                                            	<td>Samsung</td>
+                                                <td>i9105 Galaxy S2 Plus</td>
+                                                <td>£23,505.00</td>
+                                                <td>400</td>
+                                                <td><span class="label label-danger">Cancelled</span></td>
                                             </tr>
                                             </tbody>
                                         </table>
