2a4575588f224212baf9de75a22c3560eea272ae
Daniel Gregory <mail@danielgregory.me>
2015-01-26 10:15:24 +0000

    Update

--- application/modules/profile/views/whos-viewed.php
+++ application/modules/profile/views/whos-viewed.php
@@ -7,7 +7,7 @@
                             <a href="/">Home</a>
                         </li>
                         <li>
-                            My Company
+                            Profile
                         </li>
                         <li class="active">
                             <strong>Who's Viewed</strong>
@@ -23,17 +23,18 @@
                     <div class="col-sm-4">
                         <div class="text-center">
                             <img alt="image" class="img-circle m-t-xs img-responsive" src="/public/main/img/a2.jpg">
-                            <div class="m-t-xs font-bold">Graphics designer</div>
+                            <div class="m-t-xs font-bold btn-circle">Online</div>
                         </div>
                     </div>
                     <div class="col-sm-8">
                         <h3><strong>John Smith</strong></h3>
-                        <p><i class="fa fa-map-marker"></i> Riviera State 32/106</p>
+                        <p><i class="fa fa-map-marker"></i> TebiHost Limited</p>
                         <address>
-                            <strong>Twitter, Inc.</strong><br>
-                            795 Folsom Ave, Suite 600<br>
-                            San Francisco, CA 94107<br>
-                            <abbr title="Phone">P:</abbr> (123) 456-7890
+                            <strong>10 Swallow Drive</strong><br>
+                            Hazlemere<br>
+                            High Wycombe<br>
+                            Buckinghamshire, HP15 7JB<br>
+                            <abbr title="Phone">P:</abbr> +44 (0)7512 245 522
                         </address>
                     </div>
                     <div class="clearfix"></div>
