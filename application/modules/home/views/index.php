8f4ce8bb07a4ed8c4a5aaca007f634eda9f6c994
Daniel Gregory <mail@danielgregory.me>
2015-01-26 13:15:10 +0000

    Update

--- application/modules/home/views/index.php
+++ application/modules/home/views/index.php
@@ -5,11 +5,11 @@
                         <div class="ibox float-e-margins">
                             <div class="ibox-title">
                                 <span class="label label-success pull-right">Monthly</span>
-                                <h5>Income</h5>
+                                <h5>Sales</h5>
                             </div>
                             <div class="ibox-content">
-                                <h1 class="no-margins">40 886,200</h1>
-                                <div class="stat-percent font-bold text-success">98% <i class="fa fa-bolt"></i></div>
+                                <h1 class="no-margins">£886,200</h1>
+                                <div class="stat-percent font-bold text-success">23% <i class="fa fa-level-up"></i></div>
                                 <small>Total income</small>
                             </div>
                         </div>
@@ -18,23 +18,23 @@
                         <div class="ibox float-e-margins">
                             <div class="ibox-title">
                                 <span class="label label-info pull-right">Annual</span>
-                                <h5>Orders</h5>
+                                <h5>Purchases</h5>
                             </div>
                             <div class="ibox-content">
-                                <h1 class="no-margins">275,800</h1>
+                                <h1 class="no-margins">£275,800</h1>
                                 <div class="stat-percent font-bold text-info">20% <i class="fa fa-level-up"></i></div>
-                                <small>New orders</small>
+                                <small>My Purchases</small>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-3">
                         <div class="ibox float-e-margins">
                             <div class="ibox-title">
-                                <span class="label label-primary pull-right">Today</span>
-                                <h5>Vistits</h5>
+                                <span class="label label-success pull-right">Monthly</span>
+                                <h5>Visits</h5>
                             </div>
                             <div class="ibox-content">
-                                <h1 class="no-margins">106,120</h1>
+                                <h1 class="no-margins">1,320</h1>
                                 <div class="stat-percent font-bold text-navy">44% <i class="fa fa-level-up"></i></div>
                                 <small>New visits</small>
                             </div>
@@ -44,12 +44,14 @@
                         <div class="ibox float-e-margins">
                             <div class="ibox-title">
                                 <span class="label label-danger pull-right">Low value</span>
-                                <h5>User activity</h5>
+                                <h5>Feedback Score</h5>
                             </div>
                             <div class="ibox-content">
-                                <h1 class="no-margins">80,600</h1>
-                                <div class="stat-percent font-bold text-danger">38% <i class="fa fa-level-down"></i></div>
-                                <small>In first month</small>
+                            	<div class="m-r-md inline">
+                            	<input type="text" value="60" class="dial m-r" data-fgColor="#ED5565" data-width="85" data-height="85" data-angleOffset=-125 data-angleArc=250 />
+                            	</div>
+                                <div class="stat-percent font-bold text-danger">10 <i class="fa fa-level-down"></i></div>
+                                <small>Rating</small>
                             </div>
                         </div>
             </div>
