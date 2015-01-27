c4f5e6656134e49348cf0ebb81072737c2d53964
Daniel Gregory <mail@danielgregory.me>
2015-01-27 10:54:31 +0000

    Update

--- application/modules/profile/views/index.php
+++ application/modules/profile/views/index.php
@@ -62,36 +62,21 @@
                         <div class="ibox-title">
                             <h5>Company Information</h5>
                         </div>
-                        <div>
+                       	
+                        <div class="col-md-4">
+							<h4><strong>TebiHost Limited</strong></h4>
+                        </div>
+                        
+                        
+                        <div class="col-md-4">
                             <div class="ibox-content no-padding border-left-right">
                                 <img alt="image" class="img-responsive" src="/public/main/img/profile_big.jpg">
                             </div>
-                            <div class="ibox-content profile-content">
-                                <h4><strong>Daniel Gregory</strong></h4>
-                                <p>Director</p>
-                                <h5>About me</h5>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitat.
-                                </p>
-                                <div class="user-button">
-                                    <div class="row">
-                                        <div class="col-lg-6">
-                                            <button type="button" class="btn btn-primary btn-sm btn-block"><i class="fa fa-envelope"></i> Send Message</button>
-                                        </div>
-                                        <div class="col-lg-6">
-                                            <button type="button" class="btn btn-default btn-sm btn-block"><i class="fa fa-wechat"></i> Messenger</button>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-6">
-                                            <button type="button" class="btn btn-success btn-sm btn-block"><i class="fa fa-book"></i> Add Contact</button>
-                                        </div>
-                                        <div class="col-lg-6">
-                                            <button type="button" class="btn btn-warning btn-sm btn-block"><i class="fa fa-star"></i> Add Favourite</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                        </div>
+                        
+                        
+                        
+                        <div>
                     </div>
 
 
