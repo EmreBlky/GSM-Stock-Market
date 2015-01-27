644ab4d816de8d3d03cd5cfac3238d9295564fc6
NewOwner <NewOwner@NewOwner-VAIO.home>
2015-01-27 12:56:27 +0000

    Merge origin/Development into Development
    Conflicts:
    	application/config/config.php

--- database/data_cleanse/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$link = mysql_connect('109.203.125.38', 'gsmstock_admin', 'zv.4qAb17ph$;?$PF!') or die("Database Error");
-
-if(! $link )
-{
-  die('Could not connect: ' . mysql_error());
-}
-
