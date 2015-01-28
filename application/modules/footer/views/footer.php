2037375f98ad6477407c2e6ba44e1129c4c95a85
Daniel Gregory <mail@danielgregory.me>
2015-01-28 12:54:12 +0000

    Update

--- application/modules/footer/views/footer.php
+++ application/modules/footer/views/footer.php
@@ -1,6 +1,6 @@
 			<div class="footer">
             <div class="pull-right">
-                10GB of <strong>250GB</strong> Free.
+               Last login: 1/1/15 at 22:44 from the IP 86.188.109.22
             </div>
             <div>
                 <strong>Copyright</strong> GSM Stock Market Limited &copy; <?php echo date("Y"); ?>
