48a9be934f340a8bf71ffbcc02452d2464d0a054
Daniel Gregory <mail@danielgregory.me>
2015-01-28 13:20:06 +0000

    Update

--- application/modules/profile/views/index.php
+++ application/modules/profile/views/index.php
@@ -417,16 +417,8 @@
                     closeButton: $('#closeButton').prop('checked'),
                     debug: $('#debugInfo').prop('checked'),
                     progressBar: $('#progressBar').prop('checked'),
-                    positionClass: 'toast-bottom-right',
-                    onclick: null,
-					showDuration: 400,
-					hideDuration: 1000,
-					timeOut: 7000,
-					extendedTimeOut: 1000,
-					showEasing: 'swing',
-					hideEasing: 'linear',
-					showMethod: 'fadeIn',
-					hideMethod: 'fadeOut',
+                    positionClass: $('#positionGroup input:radio:checked').val() || 'toast-top-right',
+                    onclick: null
                 };
             });
         })
