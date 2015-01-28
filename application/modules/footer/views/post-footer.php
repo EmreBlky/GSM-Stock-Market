371817d719190504247d119ee0b8ca88000c307f
Daniel Gregory <mail@danielgregory.me>
2015-01-28 12:22:06 +0000

    Update

--- application/modules/footer/views/post-footer.php
+++ application/modules/footer/views/post-footer.php
@@ -237,7 +237,7 @@
 
 	<!-- notifications -->
   <a id='linkButton'>ClickMe</a>
-  <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/css/toastr.css" rel="stylesheet"/>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.js"></script>
   <script type="text/javascript">
   $(document).ready(function() {
