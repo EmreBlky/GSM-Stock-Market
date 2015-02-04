e9e47f132544c36db2e6a7167f065e2f537d16d8
NewOwner <NewOwner@NewOwner-VAIO.home>
2015-02-04 09:54:55 +0000

    Merge origin/Development into Development
    Conflicts:
    	application/modules/header/views/sidebar.php

--- application/modules/mailbox/views/dropdown.php
+++ application/modules/mailbox/views/dropdown.php
@@ -3,29 +3,33 @@
     <?php if($count > 0){?>
         <?php foreach($inbox_message as $inbox){?>
             <li>
-                <a href="mailbox/inbox/all/<?php echo $inbox->member_id; ?>" class="pull-left">
                 <div class="dropdown-messages-box">
-                    
+                    <a href="mailbox/inbox/all/<?php echo $inbox->member_id; ?>" class="pull-left">
                         <?php if(file_exists($base."public/main/images/members/'.$inbox->member_id.'.jpg")){?>
                             <img alt="image" class="img-circle" src="<?php echo $base; ?>public/main/images/members/<?php echo $inbox->member_id; ?>.jpg" height="128" width="128">
                         <?php } else {?>
                             <img alt="image" class="img-circle" src="<?php echo $base; ?>public/main/images/members/no_profile.jpg" height="128" width="128">
                         <?php }?>
-                    
+                    </a>
                     <div>
-                       
+                        <a href="mailbox/inbox/all/<?php echo $inbox->member_id; ?>">
                         <small class="pull-right"><?php echo $inbox->time; ?> <?php echo $inbox->date; ?></small>
                         <strong><?php echo $inbox->subject; ?></strong>. <br>
                         <!-- <small class="text-muted">3 days ago at 7:58 pm - 10.06.2014</small> -->
-                        
+                        </a>
                     </div>
                 </div>
-                </a>
             </li>
             <li class="divider"></li>
         <?php } ?>
     <?php }?>
-            
+    <li>
+        <div class="text-center link-block">
+            <a href="mailbox/inbox">
+                No New Messages
+            </a>
+        </div>
+    </li>        
     <li>
         <div class="text-center link-block">
             <a href="mailbox/inbox">
