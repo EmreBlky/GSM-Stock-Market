c48240491a2d790819c8e89893329f9f50c976bd
Daniel Gregory <mail@danielgregory.me>
2015-02-06 00:19:45 +0000

    Update

--- application/modules/messenger/views/index.php
+++ application/modules/messenger/views/index.php
@@ -1,8 +1,39 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+            <div class="row wrapper border-bottom white-bg page-heading">
+                <div class="col-lg-9">
+                    <h2>Messenger</h2>
+                    <ol class="breadcrumb">
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li class="active">
+                            <strong>Messenger</strong>
+                        </li>
+                    </ol>
+                </div>
+            </div>
+        <div class="wrapper wrapper-content animated fadeInRight">     
+        
+        
+        <div class="row">
+        
+         <div class="col-xs-12">
+                <div class="contact-box">
+                    <a href="messenger/chat/">
+                    <div class="col-xs-4">
+                        <div class="text-center">
+                            <img alt="image" class="img-circle m-t-xs img-responsive" src="public/main/img/daniel_big.jpg">
+                        </div>
+                    </div>
+                    <div class="col-xs-8">
+                        <h3><strong>Daniel Gregory</strong></h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec ultricies nunc. Ut vulputate felis fringilla magna eleifend, non placerat massa pharetra.....</p>
+                    </div>
+                    <div class="clearfix"></div>
+                    </a>
+                </div>
+            </div>
+            
+       	</div><!-- Row End -->  
+         
+        </div>
