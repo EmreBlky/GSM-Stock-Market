7b9402d59c713957c376ce7760eb644690f27b61
Daniel Gregory <mail@danielgregory.me>
2015-02-12 16:02:15 +0000

    Update

--- application/modules/preferences/views/password.php
+++ application/modules/preferences/views/password.php
@@ -18,18 +18,35 @@
         <div class="wrapper wrapper-content  animated fadeInRight">
             <div class="row">
                 <div class="col-lg-12">
-                    <div class="ibox ">
+                    <div class="ibox float-e-margins">
                         <div class="ibox-title">
                             <h5>Edit Password (INCOMPLETE)</h5>
                         </div>
                         <div class="ibox-content">
-                                <form role="form" id="form">
-                                    <div class="form-group"><label>Old Password</label> <input type="password" placeholder="Password" id="password" class="form-control" name="password"></div>
-                                    <div class="form-group"><label>New Password</label> <input type="password" placeholder="Password" id="password" class="form-control" name="password"></div>
-                                    <div class="form-group"><label>Repeat New Password</label> <input type="password" placeholder="Password" id="passsword_again" class="form-control" name="password_again"></div>
-                                    <div>
-                                        <button class="btn btn-sm btn-primary m-t-n-xs" type="submit"><strong>Confirm Change</strong></button>
+                            <form role="form" id="form" class="form-horizontal">
+                        		<div class="form-group"><label class="col-md-3 control-label">Current Password</label>
+                                    <div class="col-md-9">
+                                        <input type="password" placeholder="Current Password" id="password" class="form-control" name="old_password">
                                     </div>
+                                </div>  
+                        		<div class="form-group"><label class="col-md-3 control-label">New Password</label>
+                                    <div class="col-md-9">
+                                        <input type="password" placeholder="New Password" id="password" class="form-control" name="new_password">
+                                    </div>
+                                </div>  
+                        		<div class="form-group"><label class="col-md-3 control-label">Repeat New Password</label>
+                                    <div class="col-md-9">
+                                        <input type="password" placeholder="Repeat New Password" id="password" class="form-control" name="new_password_repeat">
+                                    </div>
+                                </div> 
+                                
+                                <div class="hr-line-dashed"></div>
+                                <div class="form-group">
+                                    <div class="col-md-4 col-md-offset-3">
+                                        <button class="btn btn-white" type="submit">Cancel</button>
+                                        <button class="btn btn-primary" name="submit_form" type="submit" id="submit_form">Update Password</button>
+                                    </div>
+                                </div> 
                                 </form>
                         </div>
                     </div>
@@ -41,7 +58,7 @@
         </div>
 
     <!-- Jquery Validate -->
-    <script src="js/plugins/validate/jquery.validate.min.js"></script>
+    <script src="public/main/template/core/js/plugins/validate/jquery.validate.min.js"></script>
 	<script>
 	// just for the demos, avoids form submit
 	jQuery.validator.setDefaults({
@@ -50,8 +67,8 @@
 	});
 	$( "#form" ).validate({
 	  rules: {
-		password: "required",
-		password_again: {
+		new_password: "required",
+		New_password_again: {
 		  equalTo: "#password"
 		}
 	  }
