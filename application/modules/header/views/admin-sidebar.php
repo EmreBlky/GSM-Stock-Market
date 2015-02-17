ac875c9d9bea8b910864760517fa209299c415c7
Daniel Gregory <mail@danielgregory.me>
2015-02-17 09:32:48 +0000

    Update

--- application/modules/header/views/admin-sidebar.php
+++ application/modules/header/views/admin-sidebar.php
@@ -16,32 +16,33 @@
                     </li>
                     
                     <li>
-                        <a href="profile/"><i class="fa fa-user"></i> <span class="nav-label"> My Profile</span> <span class="fa arrow"></span></a>
+                        <a href="#"><i class="fa fa-user"></i> <span class="nav-label">Edit Subscriptions</span><span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
-                            <li><a href="profile/"><i class="fa fa-user"></i> View Profile</a></li>
-                            
-                            <li><a href="profile/edit_profile"><i class="fa fa-cogs"></i> Edit Profile</a></li>
+                            <li><a href="#"><i class="fa fa-cog"></i> <span class="nav-label">User Level</span></a></li>
+                            <li><a href="#"><i class="fa fa-cog"></i> <span class="nav-label">Subscription Access</span></a></li>
                         </ul>
                     </li>
                     
-                    
-                    
                     <li>
-                        <a href="#"><i class="fa fa-wechat"></i> <span class="nav-label">Messenger</span><span class="label label-warning pull-right"></span></a></a>
-                    </li>
-                    
-                    <li>
-                        <a href="#"><i class="fa fa-book"></i> <span class="nav-label">Address Book</span></a>
-                    </li>
-                    
-                    <li>
-                        <a href="#"><i class="fa fa-line-chart"></i> <span class="nav-label">Marketplace</span> <span class="fa arrow"></span></a>
+                        <a href="#"><i class="fa fa-wechat"></i> <span class="nav-label">Profile Updates</span><span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
-                            <li><a href="marketplace/buy"><i class="fa fa-shopping-cart"></i> Buy</a></li>
-                            <li><a href="marketplace/sell"><i class="fa fa-tag"></i> Sell</a></li>
-                            <li><a href="marketplace/watching"><i class="fa fa-eye"></i> Watching</a></li>
-                            <li><a href="marketplace/listing"><i class="fa fa-list"></i> My Listings</a></li>
-                            <li><a href="marketplace/history"><i class="fa fa-file-text"></i> Order History</a></li>
+                            <li><a href="#"><i class="fa fa-cog"></i> <span class="nav-label">Company Bio</span><span class="label label-warning pull-right">7</span></a></li>
+                            <li><a href="#"><i class="fa fa-cog"></i> <span class="nav-label">Profile Feed</span><span class="label label-warning pull-right">8</span></a></li>
+                        </ul>
+                    </li>
+                    
+                    <li>
+                        <a href="#"><i class="fa fa-users"></i> <span class="nav-label">Edit Profiles</span><span class="fa arrow"></span></a></a>
+                        <ul class="nav nav-second-level">
+                            <li><a href="#"><i class="fa fa-user"></i> <span class="nav-label">Edit User</span></a></li>
+                            <li><a href="#"><i class="fa fa-users"></i> <span class="nav-label">Edit Company</span></a></li>
+                        </ul>
+                    </li>
+                    
+                    <li>
+                        <a href="#"><i class="fa fa-shoppig-cart"></i> <span class="nav-label">Marketplace</span><span class="fa arrow"></span></a></a>
+                        <ul class="nav nav-second-level">
+                            <li><a href="#"><i class="fa fa-user"></i> <span class="nav-label">Edit Listings</span></a></li>
                         </ul>
                     </li>
                     
