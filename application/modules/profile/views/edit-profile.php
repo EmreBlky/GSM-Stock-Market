a5b77fe5e108f9cb28560a4ee40a1f8583015e40
Faisal-dynamologic <Faisal@dynamologic.com>
2015-02-23 17:33:26 +0500

    Test

--- application/modules/profile/views/edit-profile.php
+++ application/modules/profile/views/edit-profile.php
@@ -912,11 +912,28 @@ echo form_open_multipart('profile/profileEdit', $attributes);
                         <div class="col-md-4">
                             <div id="primary-business">
 
+                                <?php
+                                $SelectedBiz = array();
+                                $SelectedBiz[] = $company->business_sector_1;
+                                $SelectedBiz[] = $company->business_sector_2;
+                                $SelectedBiz[] = $company->business_sector_3;
+                                $SelectedBiz[] = $other_business1;
+                                $SelectedBiz[] = $other_business2;
+                                ?>
                                 <label class="col-md-12">Primary Business <span style="color:red">*</span></label>
 
                                 <select class="form-control m-b" id="bprimary" name="bprimary" style="float:left" onchange="updateSelects1(this.value)">
-                                    <?php if (isset($company->business_sector_1)) { ?>
-                                        <option selected="selected" value="bsectors1"><?php echo $company->business_sector_1; ?></option>
+                                    <?php
+                                    if (isset($company->business_sector_1)) {
+                                        foreach ($SelectedBiz As $SelectedBizOne) {
+                                            echo $SelectedBizOne . "Faisal";
+                                            ?>
+                                            <option selected="selected" value="bsectors1"><?php echo $company->business_sector_1; ?></option>
+                                            <?php
+                                        }
+                                        ?>
+
+
                                     <?php } else { ?>
                                         <option value = "">[Select One]</option>
                                     <?php } ?>    
