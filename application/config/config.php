26416d92df74f5d6cbe78ce5a9c3952963609a96
imd-solutions <admin@imarveldesign.co.uk>
2015-02-25 10:30:30 +0000

    Merge origin/Development into Development
    Conflicts:
    	application/config/config.php

--- application/config/config.php
+++ application/config/config.php
@@ -15,10 +15,10 @@
 |
 */
 //DEVLOPMENT SITE:
-//$config['base_url']	= 'http://localhost/projects/codeigniter/gsm/gsm-secure/';
+//$config['base_url']	= 'http://localhost/gsm/gsm-secure/';
 
 //SERVER SITE:
-//$config['base_url']	= 'http://secure-dev.gsmstockmarket.com/';
+$config['base_url']	= 'http://secure-dev.gsmstockmarket.com/';
 
 //LIVE SITE:
 //$config['base_url']	= '';
