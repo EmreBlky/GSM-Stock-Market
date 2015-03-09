e5a075c3da0cdba3cd9588a96966686529ef7e0f
Daniel Gregory <mail@danielgregory.me>
2015-03-09 16:43:30 +0000

    Update

--- application/modules/marketplace/views/buy-listing.php
+++ application/modules/marketplace/views/buy-listing.php
@@ -1,63 +1,70 @@
 <div class="row wrapper border-bottom white-bg page-heading">
-    <div class="col-lg-10">
-        <h2>Listing #01234567</h2>
-        <ol class="breadcrumb">
-            <li>
-                <a href="/">Home</a>
-            </li>
-            <li>
-                Buy
-            </li>
-            <li class="active">
-                <strong>Listing</strong>
-            </li>
-        </ol>
-    </div>
-    <div class="col-lg-2">
-    
-    </div>
-</div>
+                <div class="col-lg-10">
+                    <h2>View Profile</h2>
+                    <ol class="breadcrumb">
+                        <li>
+                            <a href="home">Home</a>
+                        </li>                        
+                        <li class="active">
+                            <strong>/strong>
+                        </li>
+                    </ol>
+                </div>
+                <div class="col-lg-2">
 
-				<div class="wrapper wrapper-content animated fadeInUp">
+                </div>
+            </div>
+            <div class="row">
+            <div class="col-lg-9">
+                <div class="wrapper wrapper-content animated fadeInUp">
                     <div class="ibox">
-                              <div class="ibox-title">
-                              	<h5>Listing Details</h5>
-                              </div>
-
                         <div class="ibox-content">
-                            
                             <div class="row">
-                                <div class="col-lg-5 col-lg-offset-1">
+                                <div class="col-lg-12">
+                                    <!--<div class="alert alert-danger alert-dismissable">
+                                        <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+                                        <i class="fa fa-ban"></i> You have blocked this company. They are unable to communicate or see you in anyway on this website. <a class="alert-link" href="#">Unblock</a>.
+                                    </div> -->
+                                    
+                                    <div class="m-b-md">                                        
+                                        <h2>99999</h2>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-7">
                                     <dl class="dl-horizontal">
-                                        <dt>Status:</dt> <dd><span class="label label-primary">Active</span></dd>
-                                        <dt>Subscription:</dt> <dd> Member</dd>
+                                        <dt>
+                                        <dd>
+                                            <span>
+                                                    <img alt="image" class="img-circle" src="public/main/template/gsm/images/members/no_profile.jpg">                          
+                                            </span>
+                                        </dd>
+                                        </dt>
+                                    </dl>
+                                    <dl class="dl-horizontal">
                                     </dl>
                                     <dl class="dl-horizontal">
                                     </dl>                                    
                                     <dl class="dl-horizontal">
-                                        <dt>Address:</dt>  
+                                        <dt>Address:</dt>
                                     </dl>
                                     
                                     <dl class="dl-horizontal">
-                                    </dl>
-                                    
-                                    <dl class="dl-horizontal">
+                                        <dt>Primary Business:</dt>
                                     </dl>
                                     
                                 </div>
-                                <div class="col-lg-5 col-lg-offset-1">
-                                
-                                
+                                <div class="col-lg-5" id="cluster_info">
                               		<div class="m-r-md" style="text-align:center">
                             			<input type="text" value="94" class="dial m-r" data-fgColor="#1AB394" data-width="85" data-height="85" data-angleOffset=-125 data-angleArc=250 readonly/>
                                         <div style="display:inline;height:65px;width:65px;padding:10px;margin-left:20px;"><i class="fa fa-star" style="font-size:75px;color:#FC6;vertical-align:top"></i></div>
                             		</div>
-                                    <dl class="dl-horizontal" >
-
-                                        <dt>Date Created:</dt> <dd> </dd>
-                                        <dt>Last Online:</dt> <dd> 	10.07.2014 23:36:57 </dd>
-                                        <dt>Company Users:</dt>
+                                        
                                     </dl>
-                                    
                                 </div>
-                                </div></div>
+                            </div><!-- /row -->
+              </div></div></div></div>
+            <div class="col-lg-3">
+                                                    <img alt="image" class="img-circle" src="public/main/template/gsm/images/members/no_profile.jpg">  
+            </div>
