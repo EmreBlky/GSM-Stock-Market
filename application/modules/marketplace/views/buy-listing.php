13b616d19c4dda8a5550310e8cbbb86fbcb221ab
Daniel Gregory <mail@danielgregory.me>
2015-03-09 13:52:44 +0000

    buy listing update

--- application/modules/marketplace/views/buy-listing.php
+++ application/modules/marketplace/views/buy-listing.php
@@ -7,6 +7,7 @@
             </li>
             <li>
                 Buy
+            </li>
             <li class="active">
                 <strong>Listing</strong>
             </li>
@@ -17,21 +18,46 @@
     </div>
 </div>
 
-<div class="wrapper wrapper-content">
-	<div class="row">
-		<div class="col-lg-8">
-    	<div class="ibox float-e-margins">
-		<div class="ibox-title">
-			<h5>Product Details</h5>
+				<div class="wrapper wrapper-content animated fadeInUp">
+                    <div class="ibox">
+                              <div class="ibox-title">
+                              	<h5>Listing Details</h5>
+                              </div>
 
-</div>
-<div class="ibox-content">
+                        <div class="ibox-content">
+                            
+                            <div class="row">
+                                <div class="col-lg-5 col-lg-offset-1">
+                                    <dl class="dl-horizontal">
+                                        <dt>Status:</dt> <dd><span class="label label-primary">Active</span></dd>
+                                        <dt>Subscription:</dt> <dd> Member</dd>
+                                    </dl>
+                                    <dl class="dl-horizontal">
+                                    </dl>                                    
+                                    <dl class="dl-horizontal">
+                                        <dt>Address:</dt>  
+                                    </dl>
+                                    
+                                    <dl class="dl-horizontal">
+                                    </dl>
+                                    
+                                    <dl class="dl-horizontal">
+                                    </dl>
+                                    
+                                </div>
+                                <div class="col-lg-5 col-lg-offset-1">
+                                
+                                
+                              		<div class="m-r-md" style="text-align:center">
+                            			<input type="text" value="94" class="dial m-r" data-fgColor="#1AB394" data-width="85" data-height="85" data-angleOffset=-125 data-angleArc=250 readonly/>
+                                        <div style="display:inline;height:65px;width:65px;padding:10px;margin-left:20px;"><i class="fa fa-star" style="font-size:75px;color:#FC6;vertical-align:top"></i></div>
+                            		</div>
+                                    <dl class="dl-horizontal" >
 
-                
-
-  </div>
- </div> 
-</div>       
-       
-</div>
-</div>
+                                        <dt>Date Created:</dt> <dd> </dd>
+                                        <dt>Last Online:</dt> <dd> 	10.07.2014 23:36:57 </dd>
+                                        <dt>Company Users:</dt>
+                                    </dl>
+                                    
+                                </div>
+                                </div></div>
