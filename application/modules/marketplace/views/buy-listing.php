c6af5e4d698adb4152b3a7ac80f1967a47e85675
Daniel Gregory <mail@danielgregory.me>
2015-03-09 09:52:25 +0000

    Listing Update

--- application/modules/marketplace/views/buy-listing.php
+++ application/modules/marketplace/views/buy-listing.php
@@ -1,8 +1,37 @@
-<?php
+<div class="row wrapper border-bottom white-bg page-heading">
+    <div class="col-lg-10">
+        <h2>Listing #01234567</h2>
+        <ol class="breadcrumb">
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                Buy
+            <li class="active">
+                <strong>Listing</strong>
+            </li>
+        </ol>
+    </div>
+    <div class="col-lg-2">
+    
+    </div>
+</div>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<div class="wrapper wrapper-content">
+	<div class="row">
+		<div class="col-lg-8">
+    	<div class="ibox float-e-margins">
+		<div class="ibox-title">
+			<h5>Product Details</h5>
 
+</div>
+<div class="ibox-content">
+
+                
+
+  </div>
+ </div> 
+</div>       
+       
+</div>
+</div>
