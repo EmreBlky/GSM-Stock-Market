a1dbb10d8551fae8b674bab1648d6570293c9b29
Sourabh Nagar <sourabh.chapter247@gmail.com>
2015-03-11 14:19:18 +0000

    added mpn and isbn form field

--- application/modules/admin/views/edit_listing_attribute.php
+++ application/modules/admin/views/edit_listing_attribute.php
@@ -30,12 +30,20 @@
 
     <form method="post" action="<?php echo current_url()?>"  class="validation form-horizontal" />    	
 
-        <div class="form-group"><label class="col-md-3 control-label">Product  MPN/ISBN</label>
+        <div class="form-group"><label class="col-md-3 control-label">Product  MPN</label>
             <div class="col-md-9">
-                <input type="type" class="form-control" placeholder="MPN/ISBN Number" name="product_mpn_isbn" value="<?php if(!empty($listing_attributes->product_mpn_isbn)){ echo $listing_attributes->product_mpn_isbn; }else{ echo set_value('product_mpn_isbn'); } ?>" />
-                <?php echo form_error('product_mpn_isbn'); ?>
+                <input type="type" class="form-control" placeholder="MPN Number" name="product_mpn" value="<?php if(!empty($listing_attributes->product_mpn)){ echo $listing_attributes->product_mpn; }else{ echo set_value('product_mpn'); } ?>" />
+                <?php echo form_error('product_mpn'); ?>
             </div>
           </div>
+
+           <div class="form-group"><label class="col-md-3 control-label">Product  ISBN</label>
+            <div class="col-md-9">
+                <input type="type" class="form-control" placeholder="ISBN Number" name="product_isbn" value="<?php if(!empty($listing_attributes->product_isbn)){ echo $listing_attributes->product_isbn; }else{ echo set_value('product_isbn'); } ?>" />
+                <?php echo form_error('product_isbn'); ?>
+            </div>
+          </div>
+        
 
           <div class="form-group"><label class="col-md-3 control-label">Product Make</label>
             <div class="col-md-9">
