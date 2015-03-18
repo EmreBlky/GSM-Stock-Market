00d8c795b27c62a51b5697978cb4a652bc084fb9
NewOwner <NewOwner@NewOwner-VAIO.home>
2015-03-18 11:37:14 +0000

    Merge origin/Development into Development
    Conflicts:
    	application/modules/addressbook/views/index.php

--- application/modules/paypal/controllers/paypal.php
+++ application/modules/paypal/controllers/paypal.php
@@ -25,7 +25,7 @@ class Paypal extends MX_Controller
         $config['notify_url']           = $base .'paypal/process'; //IPN Post
         $config['production']           = FALSE; //Its false by default and will use sandbox
         //$config['discount_rate_cart']   = 20; //This means 20% discount
-        $config["invoice"]              = 'INV4'; //The invoice id
+        $config["invoice"]              = 'INV5'; //The invoice id
 
         $this->load->library('paypal_lib',$config);
 
