ae9fc2366bf0a814eb708452e36002697b49acd2
Syed Hamamd Sibtain <hammad.sibtain@dynamologic.com>
2015-03-19 19:59:11 +0500

    Crop Functionality added in edit profile page.

--- application/modules/footer/views/post-footer.php
+++ application/modules/footer/views/post-footer.php
@@ -1,30 +1,31 @@
-<script>       
+<script>
         $(function() {
 
             autoRefresh();
 
         });
-        
+
         function autoRefresh()
         {
-            
+
             setTimeout("autoRefresh()",10000);
-            
-            $.get("mailbox/messages_count", function(data) {                
-                $("#result").html(data);    
-            });  
+
+            $.get("mailbox/messages_count", function(data) {
+                $("#result").html(data);
+            });
             $.get("mailbox/new_message", function(data) {
-                $("#inbox_count").html(data);    
+                $("#inbox_count").html(data);
             });
             $.get("mailbox/mail_dropdown/3", function(data) {
-                $(".dropdown-messages").html(data);    
+                $(".dropdown-messages").html(data);
             });
-            
-         }
- </script> 	
-   
 
-    <?php 
+         }
+ </script>
+
+
+
+    <?php
 
 $this->load->module('analytics');
 $this->analytics->google_analytics('','');
