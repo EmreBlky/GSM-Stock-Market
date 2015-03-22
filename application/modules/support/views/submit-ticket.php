a5dc8f9bde7c317b206693f9a0bdac05284613ca
Daniel Gregory <mail@danielgregory.me>
2015-03-22 23:12:36 +0000

    Ticket Submission Layout

--- application/modules/support/views/submit-ticket.php
+++ application/modules/support/views/submit-ticket.php
@@ -1,8 +1,72 @@
-<?php
+            <div class="row wrapper border-bottom white-bg page-heading">
+                <div class="col-lg-10">
+                    <h2>Submit a Ticket</h2>
+                    <ol class="breadcrumb">
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            Support
+                        </li>
+                        <li class="active">
+                            <strong>Submit a Ticket</strong>
+                        </li>
+                    </ol>
+                </div>
+                <div class="col-lg-2">
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+                </div>
+            </div>
+        <div class="wrapper wrapper-content  animated fadeInRight">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <div class="ibox float-e-margins">
+                        <div class="ibox-title">
+                            <h5>Submit a Ticket</h5>
+                        </div>
+                        <div class="ibox-content">
+                        <form class="form-horizontal validation">
+                        
+						<div class="form-group" style="display:none">
+                        <label class="col-md-3 control-label">From</label>
+                        <div class="col-md-9">
+                        	<input class="form-control" name="from" value="" />
+                        </div>
+                        </div>
+                        
+						<div class="form-group">
+                        <label class="col-md-3 control-label">Subject</label>
+                        <div class="col-md-9">
+                        	<select class="form-control">
+                            	<option>General Enquiry</option>
+                            	<option>Billing &amp; Accounts</option>
+                            	<option>Abuse &amp; Complaints</option>
+                            	<option>Marketplace Issue</option>                                
+                            	<option>Website Feedback</option>
+                            	<option>Other</option>
+                            </select>
+                        </div>                      
+                    	</div>
+                        
+						<div class="form-group">
+                        <label class="col-md-3 control-label">Message</label>
+                        <div class="col-md-9">
+                        	<textarea class="form-control" rows="8"></textarea>
+                        </div>                       
+                    	</div>
 
+                        <div class="hr-line-dashed"></div>
+                        <div class="form-group">
+                            <div class="col-md-4 col-md-offset-3">
+                                <input class="btn btn-primary" name="submit_form" type="submit" id="submit_form" value="Submit Ticket"/>
+                            </div>
+                        </div>
+                        </form>
+                        </div>
+
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
