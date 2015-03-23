bd9b29900048b9f8e9b58dd1b397bf53775c2ce4
NewOwner <NewOwner@NewOwner-VAIO.home>
2015-03-23 12:52:37 +0000

    Merge origin/Development into Development
    Conflicts:
    	application/modules/login/controllers/login.php

--- application/config/config.php
+++ application/config/config.php
@@ -16,13 +16,31 @@
 */
 
 //SERVER SITE:
-$config['base_url']	= 'http://secure.gsmstockmarket.com/';
+$config['base_url']	= '';
 
 //LIVE SITE:
 //$config['base_url']	= '';
 
 //DEVLOPMENT SITE:
 //$config['base_url']	= 'http://localhost/gsm/gsm-secure/';
+
+$base_url = $_SERVER['HTTP_HOST'].$_SERVER["REQUEST_URI"];
+
+if($base_url == 'http://localhost/gsm/gsm-secure/')
+{
+	$config['base_url'] = 'http://localhost/gsm/gsm-secure/';
+}
+elseif($base_url == 'http://localhost/projects/codeigniter/gsm/gsm-secure/'){
+    
+        $config['base_url'] = 'http://localhost/projects/codeigniter/gsm/gsm-secure/';
+}
+elseif($base_url == 'http://localhost/secure.gsmstockmarket.com/'){
+    
+        $config['base_url'] = 'http://localhost/secure.gsmstockmarket.com/';    
+}
+elseif($base_url == 'http://secure-dev.gsmstockmarket.com/'){
+	$config['base_url']	= 'http://secure-dev.gsmstockmarket.com/';
+}
 
 /*
 |--------------------------------------------------------------------------
