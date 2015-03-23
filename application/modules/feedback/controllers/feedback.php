9463848be78da634765394af9bf340f88037ce44
NewOwner <NewOwner@NewOwner-VAIO.home>
2015-03-23 15:09:35 +0000

    Merge origin/Development into Development
    Conflicts:
    	application/modules/login/controllers/login.php

--- application/modules/feedback/controllers/feedback.php
+++ application/modules/feedback/controllers/feedback.php
@@ -9,6 +9,8 @@ class Feedback extends MX_Controller
         { 
             redirect('login');
         }
+        $this->load->model('member/member_model', 'member_model');
+        $this->load->model('company/company_model', 'company_model');
     }
 
     function index()
@@ -18,5 +20,20 @@ class Feedback extends MX_Controller
         $data['page'] = 'index';
         $this->load->module('templates');
         $this->templates->page($data);
-    } 
+    }
+    
+    function leave_feedback($mid)
+    {
+        $data['main'] = 'feedback';
+	$data['title'] = 'feedback';
+        $data['page'] = 'leave-feedback';
+        $data['member'] = $this->company_model->get_where($this->member_model->get_where($mid)->company_id)->company_name;
+        $this->load->module('templates');
+        $this->templates->page($data);
+    }
+    
+    function processFeedback()
+    {
+        
+    }
 }
--- application/modules/marketplace/views/open-orders.php
+++ application/modules/marketplace/views/open-orders.php
@@ -273,47 +273,10 @@
                             </div>
                             
                             
-      <div class="modal inmodal fade" id="feedback" tabindex="-1" role="dialog"  aria-hidden="true">
-          <div class="modal-dialog modal-lg">
-              <div class="modal-content">
-                  <div class="modal-header">
-                      <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                      <h4 class="modal-title">Leave Feedback</h4>
-                      <small class="font-bold"><strong >Feedback</strong> for GSMStockMarket.com Limited</small>
-                  </div>
-                  <div class="modal-body">
-                  <div class="row">
-                  <form>
-                  	<input type="text" class="form-control" placeholder="Summary of your thoughts and experience for this user" />
-                    <div class="form-group" style="margin-top:15px">
-                        <label class="col-md-5 control-label" style="margin-top:10px;text-align:right">Communication</label>
-                        <div class="col-md-7">
-                            <input class="rb-rating">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-md-5 control-label" style="margin-top:10px;text-align:right">Shipping</label>
-                        <div class="col-md-7">
-                            <input class="rb-rating">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-md-5 control-label" style="margin-top:10px;text-align:right">Quality of Goods</label>
-                        <div class="col-md-7">
-                            <input class="rb-rating">
-                        </div>
-                    </div>
-                  </form>  
-                  </div>
-                  </div>
-					
-                  <div class="modal-footer">
-                      <button type="button" class="btn btn-primary">Leave Feedback</button>
-                      <button type="button" class="btn btn-white" data-dismiss="modal">Close</button>
-                  </div>
-              </div>
-          </div>
-      </div>
+      <?php
+        $this->load->module('feedback');
+        $this->feedback->leave_feedback($this->session->userdata('members_id'));
+      ?>
             
 
     <!-- Data Tables -->
