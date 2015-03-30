90097eb8826067ea1485d0ea86900adeb6504087
NewOwner <NewOwner@NewOwner-VAIO.home>
2015-03-30 09:51:22 +0100

    Merge origin/Development into Development
    Conflicts:
    	application/modules/admin/controllers/admin.php
    	application/modules/profile/views/index.php

--- application/modules/join/controllers/join.php
+++ application/modules/join/controllers/join.php
@@ -505,6 +505,8 @@ class Join extends MX_Controller
                 $this->email->message($email_body);
 
                 $this->email->send();
+                echo $this->email->print_debugger();
+                exit;
                     
                     redirect('http://www.gsmstockmarket.com/success');
 
