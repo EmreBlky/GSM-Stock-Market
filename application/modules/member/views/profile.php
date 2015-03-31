08a5d524b4c9265eadb30e3152df65a2ed782f00
Daniel Gregory <mail@danielgregory.me>
2015-03-31 13:35:51 +0100

    Update

--- application/modules/member/views/profile.php
+++ application/modules/member/views/profile.php
@@ -187,8 +187,8 @@
 									</style>
                                     
                               		<div class="m-r-md" style="text-align:center">
-										<?php if(file_exists("public/main/template/gsm/images/company/".$member_company->id.".jpg")){?>
-                                            <img src="public/main/template/gsm/images/company/<?php echo $member_company->id; ?>.jpg" class="img-responsive" style="margin:0 auto;max-height:150px">
+										<?php if(file_exists("public/main/template/gsm/images/company/".$member_company->id.".png")){?>
+                                            <img src="public/main/template/gsm/images/company/<?php echo $member_company->id; ?>.png" class="img-responsive" style="margin:0 auto;max-height:150px">
                                         <?php } else {?>
                                             <img src="public/main/template/gsm/images/company/no_company.jpg" class="img-responsive" style="margin:0 auto;max-height:150px">
                                         <?php }?>
