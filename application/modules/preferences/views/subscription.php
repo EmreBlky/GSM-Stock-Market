dc5a7a503ea21b5f722945d2284fb9f8ea5ee4a6
NewOwner <NewOwner@NewOwner-VAIO.home>
2015-04-01 14:04:55 +0100

    Merge origin/Development into Development
    Conflicts:
    	application/modules/admin/controllers/admin.php
    	application/modules/profile/views/index.php

--- application/modules/preferences/views/subscription.php
+++ application/modules/preferences/views/subscription.php
@@ -24,7 +24,7 @@
 
                 </div>
             </div>
-        <div class="wrapper wrapper-content  animated fadeInRight">
+        <div class="wrapper wrapper-content  animated fadeInRight"> 
             <div class="row">
             
             
