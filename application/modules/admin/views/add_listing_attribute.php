d36cd62ca0dddf84541a27da7ad852eb22a4ce80
Sourabh Nagar <sourabh.chapter247@gmail.com>
2015-04-07 06:22:15 +0000

    update code

--- application/modules/admin/views/add_listing_attribute.php
+++ application/modules/admin/views/add_listing_attribute.php
@@ -110,7 +110,7 @@
 
          <div class="form-group">
             <div class="col-md-9 col-md-offset-3">
-                <a href="<?php echo base_url('admin/listing_attributes')?>">Cancel</a>
+                <a href="<?php echo base_url('admin/listing_attributes')?>" class="btn btn-danger">Cancel</a>
                 <button class="btn btn-warning" type="submit">Save Attributes</button>
             </div>
         </div>
