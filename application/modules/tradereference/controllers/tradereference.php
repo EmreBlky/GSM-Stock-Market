90c68fff5b537bb04eedb7b55543ad9e91ff75ed
NewOwner <NewOwner@NewOwner-VAIO.home>
2015-04-15 15:13:31 +0100

    Merge origin/Development into Development
    Conflicts:
    	application/modules/header/views/sidebar.php
    	application/modules/mailbox/controllers/mailbox.php
    	application/modules/mailbox/views/draft.php
    	application/modules/mailbox/views/reply.php
    	application/modules/preferences/controllers/preferences.php

--- application/modules/tradereference/controllers/tradereference.php
+++ application/modules/tradereference/controllers/tradereference.php
@@ -66,6 +66,9 @@ class Tradereference extends MX_Controller
         $this->form_validation->set_rules('trade_2_phone', 'Phone 2', 'xss_clean');
         $this->form_validation->set_rules('trade_1_country', 'Country 1', 'xss_clean');
         $this->form_validation->set_rules('trade_2_country', 'Country 2', 'xss_clean');
+        
+        $email1 = $this->input->post('trade_1_email');
+        $email2 = $this->input->post('trade_2_email');
 
             if($this->form_validation->run()){
                 
@@ -75,13 +78,13 @@ class Tradereference extends MX_Controller
                 $data = array(
                             'trade_1_company'   => $this->input->post('trade_1_company'),
                             'trade_1_name'      => $this->input->post('trade_1_name'),
-                            'trade_1_email'     => $this->input->post('trade_1_email'),
+                            'trade_1_email'     => $email1,
                             'trade_1_phone'     => $this->input->post('trade_1_phone'),
                             'trade_1_country'   => $this->input->post('trade_1_country'),
                             'trade_1_code'      => $code1,
                             'trade_2_company'   => $this->input->post('trade_2_company'),
                             'trade_2_name'      => $this->input->post('trade_2_name'),
-                            'trade_2_email'     => $this->input->post('trade_2_email'),
+                            'trade_2_email'     => $email2,
                             'trade_2_phone'     => $this->input->post('trade_2_phone'),
                             'trade_2_country'   => $this->input->post('trade_2_country'),
                             'trade_2_code'      => $code2,
@@ -89,6 +92,65 @@ class Tradereference extends MX_Controller
                 $this->tradereference_model->_update_where($data, 'member_id', $this->session->userdata('members_id'));
 
             }
+            
+            $this->load->module('emails');
+            $config = Array(
+                            'protocol' => 'smtp',
+                            'smtp_host' => 'ssl://server.gsmstockmarket.com',
+                            'smtp_port' => 465,
+                            'smtp_user' => 'noreply@gsmstockmarket.com',
+                            'smtp_pass' => 'ehT56.l}iW]I2ba3f0',
+                            'charset' => 'utf-8',
+                            'wordwrap' => TRUE,
+                            'newline' => "\r\n",
+                            'crlf'    => ""
+                        );
+
+            $this->load->library('email', $config);
+            $this->email->set_mailtype("html");
+            
+            if($email1 != ''){
+                
+                $email_body = '
+                                You have been assigned as a reference for '.$this->session->userdata('members_id').'.
+                                <br/>
+                                Please could you click on the link below:
+                                <br/>
+                                <br/>
+                                '.$this->config->item('base_url').'radereference/confirm/'.$code1.'  
+                              ';
+                
+                $this->email->from('noreply@gsmstockmarket.com', 'GSM Stockmarket Support');
+                
+                $this->email->to($email1);
+                $this->email->subject('GSM Stockmarket Trade Reference');
+                $this->email->message($email_body);
+
+                $this->email->send();
+                
+            }
+            
+            if($email2 != ''){
+                
+                $email_body = '
+                                You have been assigned as a reference for '.$this->session->userdata('members_id').'.
+                                <br/>
+                                Please could you click on the link below:
+                                <br/>
+                                <br/>
+                                '.$this->config->item('base_url').'radereference/confirm/'.$code2.'  
+                              ';
+                
+                $this->email->from('noreply@gsmstockmarket.com', 'GSM Stockmarket Support');
+                
+                $this->email->to($email2);
+                $this->email->subject('GSM Stockmarket Trade Reference');
+                $this->email->message($email_body);
+
+                $this->email->send();
+                
+            }
+        
         $this->session->set_flashdata('trade-confirmation', '<div style="margin:0 15px">    
                                                                 <div class="alert alert-success">
                                                                     Your Trade References have been updated.
@@ -97,8 +159,13 @@ class Tradereference extends MX_Controller
         redirect('tradereference/');    
     }
     
-    function confirm($code)
+    function confirm($code = NULL)
     {
+        $data['main'] = 'tradereference';
+	$data['title'] = 'GSM - Trade Reference';
+        $data['page'] = 'confirm';
         
+        $this->load->module('templates');
+        $this->templates->page($data);
     }
 }
