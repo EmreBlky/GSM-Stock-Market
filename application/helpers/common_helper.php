37daf0f854eac9d460753400d3678375337d6b33
Sourabh Nagar <sourabh.chapter247@gmail.com>
2015-04-16 13:40:26 +0000

    market place updating

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -4,7 +4,7 @@
 */
 if ( ! function_exists('clear_cache')) {
     function clear_cache(){
-		$CI =& get_instance();
+    	$CI =& get_instance();
 		$CI->output->set_header('Expires: Wed, 11 Jan 1984 05:00:00 GMT' );
 		$CI->output->set_header('Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . 'GMT');
 		$CI->output->set_header("Cache-Control: no-cache, no-store, must-revalidate");
