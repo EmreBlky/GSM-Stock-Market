1874ae6ae0b64e2da87b85c926e4470265bbecb3
Sourabh Nagar <sourabh.chapter247@gmail.com>
2015-04-20 13:06:43 +0000

    market place updating

--- application/modules/marketplace/views/order-history.php
+++ application/modules/marketplace/views/order-history.php
@@ -1,204 +1,136 @@
-    <div class="row wrapper border-bottom white-bg page-heading">
-        <div class="col-lg-10">
-            <h2>Order History</h2>
-            <ol class="breadcrumb">
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    Marketplace
-                </li>
-                <li class="active">
-                    <strong>Order History</strong>
-                </li>
-            </ol>
-        </div><!-- /col-lg-10 -->
-        <div class="col-lg-2">
-        </div><!-- /col-lg-2 spacer -->
+<div class="row wrapper border-bottom white-bg page-heading">
+<div class="col-lg-10">
+<h2>Order History</h2>
+<ol class="breadcrumb">
+    <li>
+        <a href="/">Home</a>
+    </li>
+    <li>
+        Marketplace
+    </li>
+    <li class="active">
+        <strong>Order History</strong>
+    </li>
+</ol>
+</div><!-- /col-lg-10 -->
+<div class="col-lg-2">
+</div><!-- /col-lg-2 spacer -->
+</div>
+
+<?php msg_alert(); ?>
+<div class="wrapper wrapper-content animated fadeInRight">
+<div class="row">
+<div class="col-lg-12">
+<div class="ibox float-e-margins">
+    <div class="ibox-title">
+        <h5>View Buy Transactions</h5>
     </div>
-    
-    <?php msg_alert(); ?>
-    <div class="wrapper wrapper-content animated fadeInRight">
-        <div class="row">
-            <div class="col-lg-12">
-            <div class="ibox float-e-margins">
-                <div class="ibox-title">
-                    <h5>View Transactions</h5>
-                </div>
-                <div class="ibox-content">
+    <div class="ibox-content">
 
-                <table id="marketplace" class="table table-striped table-bordered table-hover dataTables-example" >
-                <thead>
-                <tr>
-                    <th>Inv No</th>
-                    <th>Date</th>
-                    <th>Seller/Buyer</th>
-                    <th>Amount</th>
-                    <th>Tax/VAT</th>
-                    <th>Total</th>
-                    <th>Currency</th>
-                    <th>View Transaction</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>1-1</td>
-                    <td>18/02/2015</td>
-                    <td>TebiHost Limited</td>
-                    <td style="text-align:right">300.00</td>
-                    <td style="text-align:right">60.00</td>
-                    <td style="text-align:right">360.00</td>
-                    <td>GBP <i class="fa fa-gbp"></i></td>
-                    <th style="text-align:center"><a href="marketplace/invoice/"><button type="button" class="btn btn-primary" style="font-size:10px">View Transaction</button></a></th>
-                </tr>
-                <tr>
-                    <td>1-2</td>
-                    <td>19/02/2015</td>
-                    <td>Project Melon Limited</td>
-                    <td style="text-align:right">200.00</td>
-                    <td style="text-align:right">40.00</td>
-                    <td style="text-align:right">240.00</td>
-                    <td>GBP <i class="fa fa-gbp"></i></td>
-                    <th style="text-align:center"><a href="marketplace/invoice/"><button type="button" class="btn btn-primary" style="font-size:10px">View Transaction</button></a></th>
-                </tr>
-                <tr>
-                    <td>1-3</td>
-                    <td>19/02/2015</td>
-                    <td>ClickMobileShop.com Limited</td>
-                    <td style="text-align:right">150.00</td>
-                    <td style="text-align:right">30.00</td>
-                    <td style="text-align:right">180.00</td>
-                    <td>GBP <i class="fa fa-gbp"></i></td>
-                    <th style="text-align:center"><a href="marketplace/invoice/"><button type="button" class="btn btn-primary" style="font-size:10px">View Transaction</button></a></th>
-                </tr>
-                <tr>
-                    <td>1-4</td>
-                    <td>19/02/2015</td>
-                    <td>Project Melon Limited</td>
-                    <td style="text-align:right">240.00</td>
-                    <td style="text-align:right">48.00</td>
-                    <td style="text-align:right">288.00</td>
-                    <td>USD <i class="fa fa-usd"></i></td>
-                    <th style="text-align:center"><a href="marketplace/invoice/"><button type="button" class="btn btn-primary" style="font-size:10px">View Transaction</button></a></th>
-                </tr>
-                <tr>
-                    <td>1-5</td>
-                    <td>19/02/2015</td>
-                    <td>ClickMobileShop.com Limited</td>
-                    <td style="text-align:right">9800.00</td>
-                    <td style="text-align:right">NIL</td>
-                    <td style="text-align:right">9800.00</td>
-                    <td>EUR <i class="fa fa-euro"></i></td>
-                    <th style="text-align:center"><a href="marketplace/invoice/"><button type="button" class="btn btn-primary" style="font-size:10px">View Transaction</button></a></th>
-                </tr>
-                <tr>
-                    <td>1-6</td>
-                    <td>19/02/2015</td>
-                    <td>ClickMobileShop.com Limited</td>
-                    <td style="text-align:right">9800.00</td>
-                    <td style="text-align:right">NIL</td>
-                    <td style="text-align:right">9800.00</td>
-                    <td>EUR <i class="fa fa-euro"></i></td>
-                    <th style="text-align:center"><a href="marketplace/invoice/"><button type="button" class="btn btn-primary" style="font-size:10px">View Transaction</button></a></th>
-                </tr>
-                <tr>
-                    <td>1-7</td>
-                    <td>19/02/2015</td>
-                    <td>ClickMobileShop.com Limited</td>
-                    <td style="text-align:right">9800.00</td>
-                    <td style="text-align:right">NIL</td>
-                    <td style="text-align:right">9800.00</td>
-                    <td>EUR <i class="fa fa-euro"></i></td>
-                    <th style="text-align:center"><a href="marketplace/invoice/"><button type="button" class="btn btn-primary" style="font-size:10px">View Transaction</button></a></th>
-                </tr>
-                <tr>
-                    <td>1-8</td>
-                    <td>19/02/2015</td>
-                    <td>ClickMobileShop.com Limited</td>
-                    <td style="text-align:right">9800.00</td>
-                    <td style="text-align:right">NIL</td>
-                    <td style="text-align:right">9800.00</td>
-                    <td>EUR <i class="fa fa-euro"></i></td>
-                    <th style="text-align:center"><a href="marketplace/invoice/"><button type="button" class="btn btn-primary" style="font-size:10px">View Transaction</button></a></th>
-                </tr>
-                <tr>
-                    <td>1-9</td>
-                    <td>19/02/2015</td>
-                    <td>ClickMobileShop.com Limited</td>
-                    <td style="text-align:right">9800.00</td>
-                    <td style="text-align:right">NIL</td>
-                    <td style="text-align:right">9800.00</td>
-                    <td>EUR <i class="fa fa-euro"></i></td>
-                    <th style="text-align:center"><a href="marketplace/invoice/"><button type="button" class="btn btn-primary" style="font-size:10px">View Transaction</button></a></th>
-                </tr>
-                <tr>
-                    <td>1-10</td>
-                    <td>19/02/2015</td>
-                    <td>ClickMobileShop.com Limited</td>
-                    <td style="text-align:right">9800.00</td>
-                    <td style="text-align:right">NIL</td>
-                    <td style="text-align:right">9800.00</td>
-                    <td>EUR <i class="fa fa-euro"></i></td>
-                    <th style="text-align:center"><a href="marketplace/invoice/"><button type="button" class="btn btn-primary" style="font-size:10px">View Transaction</button></a></th>
-                </tr>
-                <tr>
-                    <td>1-11</td>
-                    <td>19/02/2015</td>
-                    <td>ClickMobileShop.com Limited</td>
-                    <td style="text-align:right">9800.00</td>
-                    <td style="text-align:right">NIL</td>
-                    <td style="text-align:right">9800.00</td>
-                    <td>EUR <i class="fa fa-euro"></i></td>
-                    <th style="text-align:center"><a href="marketplace/invoice/"><button type="button" class="btn btn-primary" style="font-size:10px">View Transaction</button></a></th>
-                </tr>
-                <tr>
-                    <td>1-12</td>
-                    <td>19/02/2015</td>
-                    <td>ClickMobileShop.com Limited</td>
-                    <td style="text-align:right">9800.00</td>
-                    <td style="text-align:right">NIL</td>
-                    <td style="text-align:right">9800.00</td>
-                    <td>EUR <i class="fa fa-euro"></i></td>
-                    <th style="text-align:center"><a href="marketplace/invoice/"><button type="button" class="btn btn-primary" style="font-size:10px">View Transaction</button></a></th>
-                </tr>
-                </tbody>
-                </table>
+    <table id="marketplace" class="table table-striped table-bordered table-hover dataTables-example" >
+    <thead>
+    <tr>
+        <th>Inv No</th>
+        <th>Date</th>
+        <th>Seller</th>
+        <th>Amount</th>
+        <th>Tax/VAT</th>
+        <th>Total</th>
+        <th>Currency</th>
+        <th>View Transaction</th>
+    </tr>
+    </thead>
+    <tbody>
+      <?php if(!empty($sell_order)){
+        foreach ($sell_order as $value){ ?>
+    <tr>
+        <td><?php echo $value->invoice_no;?></td>
+        <td><?php echo date('d-M-y, H:i', strtotime($value->shipping_recevied_datetime)); ?></td>
+        <td>TebiHost Limited</td>
+        <td style="text-align:right">300.00</td>
+        <td style="text-align:right">60.00</td>
+        <td style="text-align:right"><?php echo $value->total_price;?></td>
+        <td>  <?php echo currency_class($value->buyer_currency); ?>   </td>
+        <th style="text-align:center"><a href="marketplace/invoice/"<?php echo $value->id?>><button type="button" class="btn btn-primary" style="font-size:10px">View Transaction</button></a></th>
+    </tr>
+    <?php } } ?></tbody>
+    </table>
 
-                </div>
-            </div>
-        </div>
-        </div>
-        
-        </div>
+    </div>
+</div>
+</div>
 
-    <!-- Data Tables -->
-    <link href="public/main/template/core/css/plugins/dataTables/dataTables.bootstrap.css" rel="stylesheet">
-    <link href="public/main/template/core/css/plugins/dataTables/dataTables.responsive.css" rel="stylesheet">
-    <link href="public/main/template/core/css/plugins/dataTables/dataTables.tableTools.min.css" rel="stylesheet">
+<div class="col-lg-12">
+<div class="ibox float-e-margins">
+    <div class="ibox-title">
+        <h5>View Buy Transactions</h5>
+    </div>
+    <div class="ibox-content">
 
-    <!-- Data Tables -->
-    <script type="text/javascript" src="public/main/template/core/js/plugins/dataTables/jquery.dataTables.js"></script>
-    <script type="text/javascript" src="public/main/template/core/js/plugins/dataTables/dataTables.bootstrap.js"></script>
-    <script type="text/javascript" src="public/main/template/core/js/plugins/dataTables/dataTables.responsive.js"></script>
-    <script type="text/javascript" src="public/main/template/core/js/plugins/dataTables/dataTables.tableTools.min.js"></script>
+    <table id="marketplace" class="table table-striped table-bordered table-hover dataTables-example" >
+    <thead>
+    <tr>
+        <th>Inv No</th>
+        <th>Date</th>
+        <th>Seller</th>
+        <th>Amount</th>
+        <th>Tax/VAT</th>
+        <th>Total</th>
+        <th>Currency</th>
+        <th>View Transaction</th>
+    </tr>
+    </thead>
+    <tbody>
+      <?php if(!empty($buy_order)){
+        foreach ($buy_order as $value){ ?>
+    <tr>
+        <td><?php echo $value->invoice_no;?></td>
+        <td><?php echo date('d-M-y, H:i', strtotime($value->shipping_recevied_datetime)); ?></td>
+        <td>TebiHost Limited</td>
+        <td style="text-align:right">300.00</td>
+        <td style="text-align:right">60.00</td>
+        <td style="text-align:right"><?php echo $value->total_price;?></td>
+        <td>  <?php echo currency_class($value->buyer_currency); ?>   </td>
+        <th style="text-align:center"><a href="marketplace/invoice/"<?php echo $value->id?>><button type="button" class="btn btn-primary" style="font-size:10px">View Transaction</button></a></th>
+    </tr>
+    <?php } } ?></tbody>
+    </table>
 
-    <!-- Chosen -->
-    <script src="public/main/template/core/js/plugins/chosen/chosen.jquery.js"></script>
-    
-    
-    <!-- Page-Level Scripts -->
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $('.dataTables-example').dataTable({
-                responsive: true,
-                "dom": 'T<"clear">lfrtip',
-                "tableTools": {
-                    "sSwfPath": "public/main/template/core/js/plugins/dataTables/swf/copy_csv_xls_pdf.swf"
-                }
-            });
-        });
-    </script>
-    
+    </div>
+</div>
+</div>
+</div>
+
+</div>
+
+<!-- Data Tables -->
+<link href="public/main/template/core/css/plugins/dataTables/dataTables.bootstrap.css" rel="stylesheet">
+<link href="public/main/template/core/css/plugins/dataTables/dataTables.responsive.css" rel="stylesheet">
+<link href="public/main/template/core/css/plugins/dataTables/dataTables.tableTools.min.css" rel="stylesheet">
+
+<!-- Data Tables -->
+<script type="text/javascript" src="public/main/template/core/js/plugins/dataTables/jquery.dataTables.js"></script>
+<script type="text/javascript" src="public/main/template/core/js/plugins/dataTables/dataTables.bootstrap.js"></script>
+<script type="text/javascript" src="public/main/template/core/js/plugins/dataTables/dataTables.responsive.js"></script>
+<script type="text/javascript" src="public/main/template/core/js/plugins/dataTables/dataTables.tableTools.min.js"></script>
+
+<!-- Chosen -->
+<script src="public/main/template/core/js/plugins/chosen/chosen.jquery.js"></script>
+
+
+<!-- Page-Level Scripts -->
+<script type="text/javascript">
+$(document).ready(function() {
+$('.dataTables-example').dataTable({
+    responsive: true,
+    "dom": 'T<"clear">lfrtip',
+    "tableTools": {
+        "sSwfPath": "public/main/template/core/js/plugins/dataTables/swf/copy_csv_xls_pdf.swf"
+    }
+});
+});
+</script>
+
 <style>
 body.DTTT_Print { background: #fff;}
 .DTTT_Print #page-wrapper {margin: 0;background:#fff;}
