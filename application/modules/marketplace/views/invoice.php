ec1540ff923a54caf1a0d2ff2ec9640c2f7d3666
Sourabh Nagar <sourabh.chapter247@gmail.com>
2015-04-21 12:00:30 +0000

    market place updating

--- application/modules/marketplace/views/invoice.php
+++ application/modules/marketplace/views/invoice.php
@@ -1,125 +1,125 @@
-            <div class="row wrapper border-bottom white-bg page-heading">
-                <div class="col-lg-8">
-                    <h2>Transaction</h2>
-                    <ol class="breadcrumb">
-                        <li>
-                            <a href="index.html">Home</a>
-                        </li>
-                        <li>
-                            Marketplace
-                        </li>
-                        <li>
-                            <a href="marketplace/history">Order History</a>
-                        </li>
-                        <li class="active">
-                            <strong>Transaction</strong>
-                        </li>
-                    </ol>
-                </div>
-                <div class="col-lg-4">
-                    <div class="title-action">
-                        <a target="_blank" class="btn btn-primary" onClick="history.go(-1);return true;"><i class="fa fa-arrow-left"></i> Go Back </a>
-                    </div>
-                </div>
-            </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="wrapper wrapper-content animated fadeInRight">
-                    <div class="ibox-content p-xl">
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <h5>From:</h5>
-                                    <address>
-                                        <strong>ClickMobileShop.com Limited</strong><br>
-                                        Units 1 &amp; 2 The Old Dairy<br>
-                                        Hazlemere<br>
-                                       	High Wycombe<br>
-                                        Buckinghamshire. HP15 7LG<br>
-                                        United Kingdom
-                                    </address>
-                                </div>
+<div class="row wrapper border-bottom white-bg page-heading">
+<div class="col-lg-8">
+<h2>Transaction</h2>
+<ol class="breadcrumb">
+<li>
+    <a href="index.html">Home</a>
+</li>
+<li>
+    Marketplace
+</li>
+<li>
+    <a href="marketplace/history">Order History</a>
+</li>
+<li class="active">
+    <strong>Transaction</strong>
+</li>
+</ol>
+</div>
+<div class="col-lg-4">
+<div class="title-action">
+<a target="_blank" class="btn btn-primary" onClick="history.go(-1);return true;"><i class="fa fa-arrow-left"></i> Go Back </a>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-lg-12">
+<div class="wrapper wrapper-content animated fadeInRight">
+<div class="ibox-content p-xl">
+    <div class="row">
+        <div class="col-sm-6">
+            <h5>From:</h5>
+            <address>
+            <?php if($sellerinvoice=comapny_info($invoice->seller_id)){?>
+                <strong><?php echo $sellerinvoice->company_name;?></strong><br>
+                <?php echo $sellerinvoice->address_line_1;?><br>
+                <?php echo $sellerinvoice->town_city;?><br>
+                <?php echo $sellerinvoice->county;?><br>
+                <?php echo $sellerinvoice->post_code;?><br>
+                <?php echo $sellerinvoice->country;
+                }?>
+            </address>
+        </div>
 
-                                <div class="col-sm-6 text-right">
-                                    <h4>Transaction No.</h4>
-                                    <h4 class="text-navy">1-1</h4>
-                                    <span>To:</span>
-                                    <address>
-                                        <strong>GSMStockMarket.com Limited</strong><br>
-                                        Units 3 The Old Dairy<br>
-                                        Hazlemere<br>
-                                       	High Wycombe<br>
-                                        Buckinghamshire. HP15 7LG<br>
-                                        United Kingdom
-                                    </address>
-                                    <p>
-                                        <span><strong>Transaction Date:</strong> February 18, 2015</span><br/>
-                                        <span><strong>Paid Date:</strong> February 18, 2015</span>
-                                    </p>
-                                </div>
-                            </div>
+        <div class="col-sm-6 text-right">
+            <h4>Transaction No.</h4>
+            <h4 class="text-navy"><?php echo $invoice->invoice_no;?></h4>
+            <span>To:</span>
+            <address>
+                 <?php if($buyerinvoice=comapny_info($invoice->buyer_id)){?>
+                <strong><?php echo $buyerinvoice->company_name;?></strong><br>
+                <?php echo $buyerinvoice->address_line_1;?><br>
+                <?php echo $buyerinvoice->town_city;?><br>
+                <?php echo $buyerinvoice->county;?><br>
+                <?php echo $buyerinvoice->post_code;?><br>
+                <?php echo $buyerinvoice->country;
+                }?>
+            </address>
+            <p>
+                <span><strong>Transaction Date:</strong> <?php echo date('d-M-y, H:i', strtotime($invoice->shipping_recevied_datetime)); ?></span><br/>
+               
+            </p>
+        </div>
+    </div>
 
-                            <div class="table-responsive m-t">
-                                <table class="table invoice-table">
-                                    <thead>
-                                    <tr>
-                                        <th>Item List</th>
-                                        <th>Quantity</th>
-                                        <th>Unit Price</th>
-                                        <th>Tax/VAT</th>
-                                        <th>Total Price</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <td><div><strong>Apple iPhone 5 Black 16GB - LCD and Digitiser</strong></div>
-                                            <small>Replacement LCD and Digitiser, some filler text....</small></td>
-                                        <td>29</td>
-                                        <td>&pound;925.00</td>
-                                        <td>&pound;185.00</td>
-                                        <td>&pound;1,110.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td><div><strong>Shipping &amp; Handling</strong></div>
-                                            <small>DHL Tracking Number: DHL2098208</small></td>
-                                        <td>1</td>
-                                        <td>&pound;25.00</td>
-                                        <td>&pound;5.00</td>
-                                        <td>&pound;30.00</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </div><!-- /table-responsive -->
+    <div class="table-responsive m-t">
+        <table class="table invoice-table">
+            <thead>
+            <tr>
+                <th>Item List</th>
+                <th>Quantity</th>
+                <th>Unit Price</th>
+                <th>Shipping</th>
+                <th>Total Price</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td width="60%"><div><strong><?php echo $invoice->product_mpn_isbn.' '.$invoice->product_make.' '.$invoice->product_model.' '.$invoice->product_type.' '.$invoice->product_color;?></strong></div>
+                    <small><?php echo $invoice->product_desc;?></small></td>
+                <td><?php echo $invoice->product_qty;?></td>
+                <td><?php echo $invoice->unit_price;?></td>
+                <td><?php echo $invoice->shipping_price;?></td>
+                <td><?php echo $invoice->total_price;?></td>
+            </tr>
+            </tbody>
+        </table>
+    </div><!-- /table-responsive -->
 
-                            <table class="table invoice-total">
-                                <tbody>
-                                <tr>
-                                    <td><strong>Sub Total :</strong></td>
-                                    <td>&pound;950.00</td>
-                                </tr>
-                                <tr>
-                                    <td><strong>TAX/VAT :</strong></td>
-                                    <td>&pound;190.00</td>
-                                </tr>
-                                <tr>
-                                    <td><strong>TOTAL :</strong></td>
-                                    <td>&pound;1140.00</td>
-                                </tr>
-                                </tbody>
-                            </table>
-                            <p class="small">This is just a transaction receipt and not a tax/vat invoice</p>
-                            <div class="text-right">
-                        		<a href="marketplace/invoice_print" target="_blank" class="btn btn-primary"><i class="fa fa-print"></i> Print Transaction </a>
-                            </div>
-                        </div>
-                </div>
-            </div>
+    <table class="table invoice-total">
+        <tbody>
+        <tr>
+            <td><strong>Sub Total :</strong></td>
+            <td><?php echo currency_class($invoice->buyer_currency).' '.($invoice->total_price - $invoice->shipping_price);?></td>
+        </tr>
+        <!-- <tr>
+            <td><strong>TAX/VAT :</strong></td>
+            <td>&pound;190.00</td>
+        </tr> -->
+        <tr>
+            <td><strong>Shipping :</strong></td>
+            <td><?php echo currency_class($invoice->buyer_currency).' '.$invoice->shipping_price;?></td>
+        </tr>
+        <tr>
+            <td><strong>TOTAL :</strong></td>
+            <td><?php echo currency_class($invoice->buyer_currency).' '.$invoice->total_price;?></td>
+        </tr>
+        </tbody>
+    </table>
+    <p class="small">This is just a transaction receipt and not a tax/vat invoice</p>
+    <div class="text-right">
+    	<a href="marketplace/invoice_print" target="_blank" class="btn btn-primary"><i class="fa fa-print"></i> Print Transaction </a>
+    </div>
+</div>
+</div>
+</div>
 
-    <!-- Mainly scripts -->
-    <script src="js/jquery-2.1.1.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
-    <script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+<!-- Mainly scripts -->
+<script src="js/jquery-2.1.1.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
+<script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
 
-    <!-- Custom and plugin javascript -->
-    <script src="js/inspinia.js"></script>
-    <script src="js/plugins/pace/pace.min.js"></script>
+<!-- Custom and plugin javascript -->
+<script src="js/inspinia.js"></script>
+<script src="js/plugins/pace/pace.min.js"></script>
