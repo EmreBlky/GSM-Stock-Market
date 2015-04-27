8a4126f818d3e63c9c9813478b1b9639b06d5660
Daniel Gregory <mail@danielgregory.me>
2015-04-27 15:04:47 +0100

    Update

--- application/modules/events/views/attendees.php
+++ application/modules/events/views/attendees.php
@@ -1,8 +1,71 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<div class="row wrapper border-bottom white-bg page-heading">
+    <div class="col-lg-10">
+        <h2>(Event Name) Atendees</h2>
+        <ol class="breadcrumb">
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li class="active">
+                <strong>Events</strong>
+            </li>
+        </ol>
+    </div>
+    <div class="col-lg-2">
 
+    </div>
+</div>
+
+
+<div class="wrapper wrapper-content  animated fadeInRight">
+
+    <div class="row">
+    
+        <div class="col-lg-6"><!-- Profile Widget Start -->
+                <div class="contact-box">
+                    <a href="member/profile/">
+                    <div class="col-sm-5">
+                        <div class="text-center">
+                                <img alt="image" class="img-circle m-t-xs img-responsive fullhw" src="public/main/template/gsm/images/company/no_company.jpg" height="128" width="128"/>
+                        </div>
+                    </div>
+                    </a>
+                    <div class="col-sm-7 profile-minh">
+                    	<div class="col-sm-12 nopadding">
+                    	<div class="col-sm-11 nopadding">
+                        <h3 style="margin-bottom:0"><strong>GSMStockMarket.com Limited</strong></h3>
+                        <strong>First Last Name</strong><br />
+                        User Position
+                        </div>                
+                        
+                        <div class="col-sm-1" style="padding:5px 0">
+                            <img alt="image" src="public/main/template/gsm/img/flags/United_Kingdom.png" title="United Kingdom">
+                        </div>
+                        </div>
+                        
+                    	<div class="col-sm-12 nopadding">
+                        
+                        <h4 style="margin:15px 0 0 0"><strong>Business Sectors</strong></h4>
+                        <ul style="list-style:none;padding:0">                            
+                            <li>Sector 1</li>
+                            <li>Sector 2</li>
+                            <li>Sector 3</li>
+                        </ul>
+                        </div>
+                        
+                    </div>
+                    <div class="col-sm-12 gsm-contact">                      
+                            
+                            <div>
+                            <button class="btn btn-message" type="button" data-toggle="modal" data-target="#profile_message_" value=""><i class="fa fa-envelope"></i>&nbsp;Message</button>
+                            <button  onclick="location.href='member/profile/'" class="btn btn-profile" type="button"><i class="fa fa-user"></i>&nbsp;View Profile</button>
+                            </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div><!-- Profile Widget End -->
+        
+        
+    </div>
+
+</div>
