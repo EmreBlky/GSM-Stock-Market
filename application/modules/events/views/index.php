a3aa99ab3303a23b50f0abb89182339d86da4c57
Daniel Gregory <mail@danielgregory.me>
2015-04-28 08:55:14 +0100

    Height Fix

--- application/modules/events/views/index.php
+++ application/modules/events/views/index.php
@@ -32,19 +32,21 @@
             <div class="col-lg-6">
                 <div class="contact-box">
                             <div class="row">
-                        <div class="col-md-4">
-                            <?php if(file_exists("public/main/template/gsm/images/events/".$event->id.".jpg")){?>
-                                <img class="img-responsive" src="public/main/template/gsm/images/events/<?php echo $event->id;?>.jpg">
-                            <?php } else {?>
-                                <img class="img-responsive" src="public/main/template/gsm/images/events/no_image.png">
-                            <?php } ?>    
-                        </div>
-                      <div class="col-md-8 equal_height">
+                      <div class="col-md-4">
+                          <?php if(file_exists("public/main/template/gsm/images/events/".$event->id.".jpg")){?>
+                              <img class="img-responsive" src="public/main/template/gsm/images/events/<?php echo $event->id;?>.jpg">
+                          <?php } else {?>
+                              <img class="img-responsive" src="public/main/template/gsm/images/events/no_image.png">
+                          <?php } ?>    
+                      </div>
+                      <div class="col-md-8 element">
                           <h3 style="margin:0"><strong><?php echo $event->name;?></h3>
-                          <p class="text-navy"><?php echo $event->location;?>, <?php echo $event->venue;?> - <?php echo $event->date;?></strong></p>
-                          <p><?php echo $event->description;?></p> 
+                          <p class="text-navy"><i class="fa fa-map-marker text-center" style="width:1em"></i> <?php echo $event->location;?>, <?php echo $event->venue;?><br /><i class="fa fa-calendar text-center" style="width:1em"></i> <?php echo $event->date;?></strong></p>
+                          <p><?php echo $event->description;?></p>
+                      </div>
+                      <div class="col-md-12">
                           <a href="events/attendees/<?php echo $event->id;?>"><button class="btn btn-info pull-right" type="button" style="font-size:10px"><i class="fa fa-users"></i>&nbsp;View Atendees (12)</button></a>
-                          <a href="<?php echo $event->website;?>"><button class="btn btn-primary pull-right" type="button" style="font-size:10px;margin-right:15px"><i class="fa fa-globe"></i>&nbsp;Visit Website</button></a>
+                          <a href="<?php echo $event->website;?>" target="_blank"><button class="btn btn-primary pull-right" type="button" style="font-size:10px;margin-right:15px"><i class="fa fa-globe"></i>&nbsp;Visit Website</button></a>
                       </div>
                     </div>
                 </div>
@@ -82,14 +84,11 @@
 
 </div>
 
-<script>
-$( document ).ready(function() {
-    var heights = $(".equal_height").map(function() {
-        return $(this).height();
-    }).get(),
 
-    maxHeight = Math.max.apply(null, heights);
+<script type="text/javascript" src="public/main/template/gsm/js/grids.min.js"></script>
 
-    $(".equal_height").height(maxHeight);
-});
+<script type="text/javascript">
+	jQuery(function($) {
+		$('.element').responsiveEqualHeightGrid();	
+	});
 </script>
