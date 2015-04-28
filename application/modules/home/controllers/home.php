50eb71dcb97a39cd08ed1b0757ca627ae93e9b9f
Daniel Gregory <mail@danielgregory.me>
2015-04-28 12:01:50 +0100

    Phone fix

--- application/modules/home/controllers/home.php
+++ application/modules/home/controllers/home.php
@@ -338,7 +338,7 @@ class Home extends MX_Controller
                                 </tr>
                                 <tr>
                                 <td class="content-block">
-                                <p>If you need any assistance please call us on +44 (0)1494 717236 or use the online ticketing customer support within your account and we’ll be happy to help you.</p>
+                                <p>If you need any assistance please call us on +44 (0)1494 717321 or use the online ticketing customer support within your account and we’ll be happy to help you.</p>
                                 <p>Many Thanks,<br />
                                 GSMStockMarket Team</p>
                                 </td>
