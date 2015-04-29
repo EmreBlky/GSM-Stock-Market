d4144590c95c5443ac97951209010d9a3882a349
NewOwner <NewOwner@NewOwner-VAIO>
2015-04-29 10:07:43 +0100

    Merge origin/Development into Development
    Conflicts:
    	application/modules/tradereference/views/submit-refs.php

---
+++ company_image.php
@@ -0,0 +1,30 @@
+<?php
+
+//mysql_connect('109.203.125.38', 'gsmstock_admin', 'zv.4qAb17ph$;?$PF!');
+//mysql_select_db('gsmstock_securelive');
+
+mysql_connect('109.203.125.38', 'gsmstock_admin', 'zv.4qAb17ph$;?$PF!');
+mysql_select_db('gsmstock_secure');
+
+//mysql_connect('localhost', 'root', 'Rwt189K72');
+//mysql_select_db('gsmstock_secure');
+
+function image_name($name){
+    
+    $name = ltrim($name, 'public/main/template/gsm/images/company/');
+    $name = rtrim($name, '.png');
+    
+    return $name;
+}
+
+foreach(glob('public/main/template/gsm/images/company/*.*') as $filename){
+    
+    if(is_numeric(image_name($filename))){
+        //echo image_id(image_name($filename)).'<br/>';
+        //echo image_name($filename).'<br/>';
+        mysql_query("UPDATE company SET photo = 'yes' WHERE id = '".image_name($filename)."'") or die (mysql_error());
+    
+    }
+    
+}
+
