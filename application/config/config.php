ab80b91ba372de72907ef0d09f6a8e0b6ef43a77
Daniel Gregory <mail@danielgregory.me>
2015-04-30 08:15:51 +0100

    revert

--- application/config/config.php
+++ application/config/config.php
@@ -47,9 +47,6 @@ elseif($base_url == 'http://secure.gsmstockmarket.com/'){
 elseif($base_url == 'https://secure.gsmstockmarket.com/'){
 	$config['base_url']	= 'https://secure.gsmstockmarket.com/';
 }
-elseif($base_url == 'http://secure-staging.gsmstockmarket.com/'){
-	$config['base_url']	= 'http://secure-staging.gsmstockmarket.com/';
-}
 
 /*
 |--------------------------------------------------------------------------
