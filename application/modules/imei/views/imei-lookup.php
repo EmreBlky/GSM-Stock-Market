49201ac54cbcf652c793e8bbb94776af9445a9ab
Daniel Gregory <mail@danielgregory.me>
2015-05-09 10:22:11 +0100

    Update

--- application/modules/imei/views/imei-lookup.php
+++ application/modules/imei/views/imei-lookup.php
@@ -169,3 +169,144 @@
         });
     });
 </script>
+
+
+  <div class="row wrapper border-bottom white-bg page-heading">
+    <div class="col-lg-12">
+      <h2>Mobile Unlocking</h2>
+          <ol class="breadcrumb">
+          <li><a href="/">Home</a></li>
+          <li>IMEI Services</li>
+          <li class="active"><strong>Unlocking</strong></li>
+        </ol>
+    </div>
+  </div>
+
+  <div class="wrapper wrapper-content animated fadeInRight">
+      <div class="alert alert-warning" style="margin-bottom:10px;">
+      <p>This feature is currently unavailable. The IMEI services will launch soon</p>
+    </div>
+    
+      <div class="alert alert-danger" style="margin-bottom:10px;">
+      <p>Insufficient fund in your IMEI Services account. <a class="alert-link" href="imei/top_up">Top up now.</a></p>
+    </div>
+  
+    <div class="row">
+    <form class="form-horizontal validation"> 
+    
+      <div class="col-lg-12">
+        <div class="ibox">
+          <div class="ibox-title"><h5>IMEI HPI &amp; Unlocking Services</h5></div>
+          
+          <div class="ibox-content">
+               
+            <div class="form-group">
+              <label class="col-lg-3 col-lg-offset-1 control-label">Service <span style="color:red">*</span></label>
+              <div class="col-lg-6">
+              	<select id="service" class="form-control">
+                  <optgroup label="0">
+                  <option value="0" id="service-0-0">Please Select Service ....</option>
+                  </optgroup>
+                  <optgroup label="Device Check Services">
+                  <option value="1-62" id="service-1-0">1-62 - ImeiHPI Check (0.10 Credits)</option>
+                  <option value="1-129" id="service-1-1">1-129 - ImeiHPI Bulk Check (0.10 Credits)</option>
+                  </optgroup>
+                  </select>
+              </div>
+            </div>
+               
+            <div class="imei_input form-group" id="1-62">
+              <label class="col-lg-3 col-lg-offset-1 control-label">IMEI <span style="color:red">*</span></label>
+              <div class="col-lg-6">
+              	<input type="text" id="1-62" class="form-control imei_single" maxlength="15" name="imei">
+              </div>
+            </div>
+               
+            <div class="imei_input form-group" id="1-129">
+              <label class="col-lg-3 col-lg-offset-1 control-label">IMEI Bulk<span style="color:red">*</span><br /><p class="text-navy"></p></label>
+              <div class="col-lg-6">
+              	<textarea class="form-control" name="imei" rows="5"></textarea>
+              	<button class="btn btn-primary imei_bulk pull-right" style="margin-top:10px">.CSV Bulk Import IMEI</button>
+              </div>
+            </div>
+               
+            <div class="form-group">              
+              <label class="col-lg-3 col-lg-offset-1 control-label">Optional Reference</label>
+              <div class="col-lg-6">
+              	<input type="text" class="form-control">
+              </div>              
+            </div>
+               
+            <div class="form-group">
+              <label class="col-lg-3 col-lg-offset-1 control-label">Optional Notes</label>
+              <div class="col-lg-6">
+              	<textarea class="form-control" rows="5"></textarea>
+              </div>
+            </div>            
+            
+            <div class="hr-line-dashed"></div>
+            <div class="form-group">
+              <div class="col-lg-3 col-lg-offset-4">
+              	<label> <input type="checkbox" class="i-checks" name="agree" required> Confirm charge of &pound;00.00 (whatever total is)</label>
+              </div>
+              <div class="col-lg-3">
+              	<button class="btn btn-primary pull-right">Place Order</button>
+              </div>
+            </div>
+            
+          </div><!-- Ibox Content -->
+        </div>        
+      </div><!-- /col -->
+    
+    </form>  
+    </div>  <!-- /row -->
+    
+          
+  </div><!-- /Wrapper -->            
+  
+<script>
+$(document).ready(function () {
+  $('.imei_input').hide();
+  $('#1-62').show();
+  $('#service').change(function () {
+    $('.imei_input').hide();
+    $('#'+$(this).val()).show();
+  })
+});
+</script>
+
+
+    <link href="public/main/template/core/css/plugins/iCheck/custom.css" rel="stylesheet">
+
+  <!-- iCheck -->
+  <script src="public/main/template/core/js/plugins/iCheck/icheck.min.js"></script>
+  <script>
+      $(document).ready(function () {
+          $('.i-checks').iCheck({
+              checkboxClass: 'icheckbox_square-green',
+              radioClass: 'iradio_square-green',
+          });
+      });
+  </script>
+
+<!-- Jquery Validate -->
+<script src="public/main/template/core/js/plugins/validate/jquery.validate.min.js"></script>
+
+<script>
+    $(document).ready(function () {
+
+        $(".validation").validate({
+            rules: {
+                imei: {
+                    required: true,
+                    digits: true,
+                    minlength: 15,
+					maxlength: 15
+                },
+                agree: {
+                    required: true
+                }
+            }
+        });
+    });
+</script>
