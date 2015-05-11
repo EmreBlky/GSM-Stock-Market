8dc4e332edbc9a29a001e4882e59d3dc4ad3e2cb
Sourabh Nagar <sourabh.chapter247@gmail.com>
2015-05-11 09:58:46 +0000

    market place updating

--- application/modules/marketplace/views/buy.php
+++ application/modules/marketplace/views/buy.php
@@ -270,7 +270,7 @@ endif;
 <div class="col-lg-12">
 <div class="ibox float-e-margins">
 <div class="ibox-title">
-    <h5>Live Marketplace - Buying Request</h5>
+    <h5>Live Marketplace - Selling Offer</h5>
 </div>
 <div class="ibox-content">
 
