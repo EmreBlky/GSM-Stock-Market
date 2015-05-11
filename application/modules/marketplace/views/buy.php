8c8feb0d972356cba8b42a28fe077025fb74b06f
Sourabh Nagar <sourabh.chapter247@gmail.com>
2015-05-11 09:48:17 +0000

    market place updating

--- application/modules/marketplace/views/buy.php
+++ application/modules/marketplace/views/buy.php
@@ -936,7 +936,7 @@ $('#reset').click(function(){
 <div class="col-lg-12">
 <div class="ibox float-e-margins">
 <div class="ibox-title">
-<h5>Live Marketplace - Selling Offers</h5>
+<h5>Live Marketplace - Buying Request</h5>
 </div>
 <div class="ibox-content">
 
