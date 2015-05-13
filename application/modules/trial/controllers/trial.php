619c0834eccd5dbee4fca121fe9fb7b6427db083
NewOwner <NewOwner@NewOwner-VAIO>
2015-05-13 15:32:19 +0100

    Merge origin/Development into Development
    Conflicts:
    	application/modules/tradereference/views/submit-refs.php

--- application/modules/trial/controllers/trial.php
+++ application/modules/trial/controllers/trial.php
@@ -5,11 +5,9 @@ class Trial extends MX_Controller
     function __construct()
     {
         parent::__construct();
-//        if ( ! $this->session->userdata('logged_in'))
-//        { 
-//            redirect('login');
-//        }
+        
         $this->load->model('trial/trial_model', 'trial_model');
+        $this->load->model('mailbox/mailbox_model', 'mailbox_model');
         $this->load->model('member/member_model', 'member_model');
     }
 
@@ -39,6 +37,94 @@ class Trial extends MX_Controller
             $this->member_model->_update($mid, $data_mem);
             
             $this->session->set_userdata('membership', 2);
+            
+            $data = array(
+                                    'member_id'         => 5,
+                                    'member_name'       => 'GSM Support',
+                                    'sent_member_id'    => $mid,
+                                    'sent_member_name'  => $this->member_model->get_where($mid)->firstname.' '.$this->member_model->get_where($mid)->lastname,
+                                    'subject'           => 'Free Trail Activated',
+                                    'body'              => 'Congratulations. Your free 30 day trial has been activated',
+                                    'inbox'             => 'yes',
+                                    'sent'              => 'yes',
+                                    'sent_belong'       => 5,
+                                    'draft'             => 'no',
+                                    'date'              => date('d-m-Y'),
+                                    'time'              => date('H:i'),
+                                    'sent_from'         => 'support',
+                                    'parent_id'         => $this->input->post('parent_id'),
+                                    'datetime'          => date('Y-m-d H:i:s')
+                                  );
+            $this->mailbox_model->_insert($data);
+            
+            $this->load->module('emails');
+            $config = Array(
+                            'protocol' => 'smtp',
+                            'smtp_host' => 'ssl://server.gsmstockmarket.com',
+                            'smtp_port' => 465,
+                            'smtp_user' => 'noreply@gsmstockmarket.com',
+                            'smtp_pass' => 'ehT56.l}iW]I2ba3f0',
+                            'charset' => 'utf-8',
+                            'wordwrap' => TRUE,
+                            'newline' => "\r\n",
+                            'crlf'    => ""
+
+                        );
+
+            $this->load->library('email', $config);
+            $this->email->set_mailtype("html");
+            
+            $email_body = '
+                            <table class="body-wrap" style="margin: 0;padding: 0;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;background-color: #f6f6f6;width: 100%;">
+                                <tr style="margin: 0;padding: 0;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;">
+                                    <td style="margin: 0;padding: 0;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;vertical-align: top;"></td>
+                                    <td class="container" width="600" style="margin: 0 auto !important;padding: 0;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;vertical-align: top;display: block !important;max-width: 600px !important;clear: both !important;">
+                                        <div class="content" style="margin: 0 auto;padding: 20px;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;max-width: 600px;display: block;">
+                                            <table class="main" width="100%" cellpadding="0" cellspacing="0" style="margin: 0;padding: 0;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;background: #fff;border: 1px solid #e9e9e9;border-radius: 3px;">
+                                                <tr style="margin: 0;padding: 0;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;">
+                                                    <td class="content-wrap" style="margin: 0;padding: 20px;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;vertical-align: top;">
+                                                        <table cellpadding="0" cellspacing="0" style="margin: 0;padding: 0;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;">
+                                                            <tr style="margin: 0;padding: 0;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;">
+                                                                <td style="margin: 0;padding: 0;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;vertical-align: top;">
+                                                                    <img class="img-responsive" src="https://secure.gsmstockmarket.com/public/main/template/gsm/images/email/header.png" style="margin: 0;padding: 0;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;max-width: 100%;">
+                                                                </td>
+                                                            </tr>
+                                                            <tr style="margin: 0;padding: 0;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;">
+                                                                <td class="content-block" style="margin: 0;padding: 0 0 20px;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;vertical-align: top;">
+                                                                    <h3 style="margin: 40px 0 0;padding: 0;font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, &quot;Lucida Grande&quot;, sans-serif;box-sizing: border-box;font-size: 18px;color: #000;line-height: 1.2;font-weight: 400;">You have activated you 30 day free trial</h3>
+                                                                </td>
+                                                            </tr>
+                                                            <tr style="margin: 0;padding: 0;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;">
+                                                                <td class="content-block" style="margin: 0;padding: 0 0 20px;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;vertical-align: top;">
+                                                                    <p style="margin: 0;padding: 0;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;margin-bottom: 10px;font-weight: normal;">Dear '.$this->member_model->get_where($mid)->firstname.',</p>
+                                                                    <p style="margin: 0;padding: 0;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;margin-bottom: 10px;font-weight: normal;">You now have silver access to the website. This will be for 30 days starting from the day you receive this email.</p>
+                                                                </td>
+                                                            </tr>
+                                                            <tr style="margin: 0;padding: 0;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;">
+                                                                <td class="content-block" style="margin: 0;padding: 0 0 20px;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;vertical-align: top;">
+                                                                    Please feel free to access your account and enjoy the benefits of a silver member.
+                                                                </td>
+                                                            </tr>
+
+                                                          </table>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                                    </div>
+                                    </td>
+                                    <td style="margin: 0;padding: 0;font-family: &quot;Helvetica Neue&quot;, &quot;Helvetica&quot;, Helvetica, Arial, sans-serif;box-sizing: border-box;font-size: 14px;vertical-align: top;"></td>
+                                </tr>
+                            </table>
+                          ';
+
+            $this->email->from('noreply@gsmstockmarket.com', 'GSM Stockmarket Support');
+
+            //$list = array('tim@gsmstockmarket.com', 'info@gsmstockmarket.com');
+            $this->email->to($this->member_model->get_where($mid)->email);
+            $this->email->subject('30 Day Free Trial');
+            $this->email->message($email_body);
+
+            $this->email->send();
             
             $this->session->set_flashdata('confirm', '<div style="margin-top:15px">    
                                                                 <div class="alert alert-success">
