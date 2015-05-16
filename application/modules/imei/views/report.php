46cd4727d98b93ec16d2f3b1b9133b295e024be1
Daniel Gregory <mail@danielgregory.me>
2015-05-17 00:27:01 +0100

    multiselect jquery removal

--- application/modules/imei/views/report.php
+++ application/modules/imei/views/report.php
@@ -141,18 +141,7 @@ $(document).ready(function() {
         "tableTools": {
             "sSwfPath": "public/main/template/core/js/plugins/dataTables/swf/copy_csv_xls_pdf.swf"
         }
-    });
-	/* multi select */
-	var config = {
-        '.chosen-select'           : {search_contains:true},
-        '.chosen-select-deselect'  : {allow_single_deselect:true},
-        '.chosen-select-no-single' : {disable_search_threshold:10},
-        '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
-        '.chosen-select-width'     : {width:"95%"}
-    }
-    for (var selector in config) {
-        $(selector).chosen(config[selector]);
-    }			
+    });		
 
 });
 </script>
