226df60438c2d25073ba6348e07fb578c2fd96ed
Daniel Gregory <mail@danielgregory.me>
2015-05-18 14:25:07 +0100

    database name change

--- application/modules/imei/views/index.php
+++ application/modules/imei/views/index.php
@@ -1,3 +1,4 @@
+
 			<div class="row wrapper border-bottom white-bg page-heading">
                 <div class="col-lg-12">
                     <h2>IMEI Services</h2>
@@ -15,14 +16,16 @@
             <div class="col-lg-12">
                 <div class="wrapper wrapper-content animated fadeInRight">
                     <div class="alert alert-warning" style="margin-bottom:10px;">
+                    	IMEI Services Coming Soon
+                    </div>
+                    <!--
                       <p>Terms of service for mobicode here</p>
                       <button class="btn btn-primary" id='create-mobicode-account'>Accept (Create Account)</button>
                       <a href=""><button class="btn btn-danger">Decline</button></a>
                     </div>
-                   
+                    -->
                 </div>
             </div>
-        
       <script type="text/javascript">
       $(document).ready(function(){
         $('#create-mobicode-account').click(function(e)
