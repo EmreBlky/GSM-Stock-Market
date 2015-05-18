54ecd5bc16870d88e23215f3c29a5f10c46da5cd
Daniel Gregory <mail@danielgregory.me>
2015-05-18 13:07:21 +0100

    topup value change

--- application/modules/header/views/sidebar.php
+++ application/modules/header/views/sidebar.php
@@ -351,7 +351,7 @@ if($member->date_activated < '2015-05-15'){
 		<li><a href="imei/imei_lookup/"><i class="fa fa-eye"></i> IMEI HPI</a></li><!--
 		<li><a href="imei/unlocking/"><i class="fa fa-unlock-alt"></i> Unlocking</a></li>-->
 		<li><a href="imei/archive/"><i class="fa fa-book"></i> Archive</a></li>
-		<li><a href="imei/top_up/"><i class="fa fa-money"></i> <span class="nav-label">Top up</span><span class="label label-primary pull-right">5.00</span></a></li>
+		<li><a href="imei/top_up/"><i class="fa fa-money"></i> <span class="nav-label">Top up</span><span class="label label-primary pull-right">0.00</span></a></li>
 </ul>
 </li>     
 <?php } else {?>
@@ -363,7 +363,7 @@ if($member->date_activated < '2015-05-15'){
 		<li><a href="imei/imei_lookup/"><i class="fa fa-eye"></i> IMEI HPI</a></li><!--
 		<li><a href="imei/unlocking/"><i class="fa fa-unlock-alt"></i> Unlocking</a></li>-->
 		<li><a href="imei/archive/"><i class="fa fa-book"></i> Archive</a></li>
-		<li><a href="imei/top_up/"><i class="fa fa-money"></i> <span class="nav-label">Top up</span><span class="label label-primary pull-right">5.00</span></a></li>
+		<li><a href="imei/top_up/"><i class="fa fa-money"></i> <span class="nav-label">Top up</span><span class="label label-primary pull-right">0.00</span></a></li>
 </ul>
 </li>   
 <?php } ?>
