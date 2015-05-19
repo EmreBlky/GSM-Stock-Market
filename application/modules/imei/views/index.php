754d06d7e0239616d14cd8c0f8fd077bb7cf6857
Daniel Gregory <mail@danielgregory.me>
2015-05-19 23:01:01 +0100

    live tutorials

--- application/modules/imei/views/index.php
+++ application/modules/imei/views/index.php
@@ -15,9 +15,7 @@
             <div class="col-lg-12">
                 <div class="wrapper wrapper-content animated fadeInRight">
                     <div class="alert alert-warning" style="margin-bottom:10px;">
-                      <p>Terms of service for mobicode here</p>
-                      <button class="btn btn-primary" id='create-mobicode-account'>Accept (Create Account)</button>
-                      <a href=""><button class="btn btn-danger">Decline</button></a>
+                      <p>IMEI Services Coming Soon...</p>
                     </div>
                    
                 </div>
