62a69b72c76f35881bf92f33ea2db0ccec743672
Shujaat Hussain <shujaat.hussain@dynamologic.com>
2015-05-21 13:01:57 +0500

    no message

--- public/main/template/gsm/js/common.js
+++ public/main/template/gsm/js/common.js
@@ -1,5 +1,18 @@
 function ValidatePaymentInformation()
 {
-    console.log('yes');
+    var ValidationResults = false;
+    var PaymentInformation = $('#payment_info').val();
+    if (PaymentInformation == '' || PaymentInformation.trim())
+    {
+        $("#payment_info").addClass('errorClass');
+        ValidationResults = false;
+    }
+    else
+    {
+        $("#payment_info").removeClass('errorClass');
+        ValidationResults = true;
+
+    }
     return false;
+
 }
